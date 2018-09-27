cce4e1182a51872ed927de6072da2473b43e7a35
Travis Briggs <audiodude@gmail.com>
2018-09-27 18:16:49 +0000

    Replace remaining usages of Binary with MySQL BINARY

--- lucky/models/wp10/release.py
+++ lucky/models/wp10/release.py
@@ -1,4 +1,5 @@
-from sqlalchemy import Column, Binary
+from sqlalchemy import Column
+from sqlalchemy.dialects.mysql import BINARY
 from sqlalchemy.ext.declarative import declarative_base
 
 _Base = declarative_base()
@@ -6,9 +7,9 @@ _Base = declarative_base()
 class Release(_Base):
   __tablename__ = 'lucky_releases'
 
-  article = Column('rel_article', Binary(255), primary_key=True)
-  category = Column('rel_0p5_category', Binary(63))
-  timestamp = Column('rel_0p5_timestamp', Binary(20))
+  article = Column('rel_article', BINARY(255), primary_key=True)
+  category = Column('rel_0p5_category', BINARY(63))
+  timestamp = Column('rel_0p5_timestamp', BINARY(20))
 
   def __repr__(self):
     return "<Release(value=%r, article=%r, timestamp=%r)>" % (
--- lucky/models/wp10/review.py
+++ lucky/models/wp10/review.py
@@ -1,4 +1,5 @@
-from sqlalchemy import Column, Binary
+from sqlalchemy import Column
+from sqlalchemy.dialects.mysql import BINARY
 from sqlalchemy.ext.declarative import declarative_base
 
 _Base = declarative_base()
@@ -6,9 +7,9 @@ _Base = declarative_base()
 class Review(_Base):
   __tablename__ = 'lucky_reviews'
 
-  value = Column('rev_value', Binary(10))
-  article = Column('rev_article', Binary(255), primary_key=True)
-  timestamp = Column('rev_timestamp', Binary(20))
+  value = Column('rev_value', BINARY(10))
+  article = Column('rev_article', BINARY(255), primary_key=True)
+  timestamp = Column('rev_timestamp', BINARY(20))
 
   def __repr__(self):
     return "<Review(value=%r, article=%r, timestamp=%r)>" % (
