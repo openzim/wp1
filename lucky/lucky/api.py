f3299fc95652911d0bd5bc29abdd3b758b7debb2
Travis Briggs <audiodude@gmail.com>
2018-09-30 23:19:32 +0000

    Allow for empty credentials, so that tests can be run without them. Also assures that no test will hit live services.

--- lucky/lucky/api.py
+++ lucky/lucky/api.py
@@ -1,11 +1,15 @@
 import mwclient
 
-from lucky.credentials import API_CREDS
-
 _ua = 'WP1.0Bot/3.0. Run by User:Audiodude. Using mclient/0.9.1'
-site = mwclient.Site('en.wikipedia.org',
-                     clients_useragent=_ua,
-                     consumer_token=API_CREDS['CONSUMER_TOKEN'],
-                     consumer_secret=API_CREDS['CONSUMER_SECRET'],
-                     access_token=API_CREDS['ACCESS_TOKEN'],
-                     access_secret=API_CREDS['ACCESS_SECRET'])
+
+try:
+  from lucky.credentials import API_CREDS
+  site = mwclient.Site('en.wikipedia.org',
+                       clients_useragent=_ua,
+                       consumer_token=API_CREDS['CONSUMER_TOKEN'],
+                       consumer_secret=API_CREDS['CONSUMER_SECRET'],
+                       access_token=API_CREDS['ACCESS_TOKEN'],
+                       access_secret=API_CREDS['ACCESS_SECRET'])
+except ImportError:
+  site = None
+
--- lucky/lucky/wiki_db.py
+++ lucky/lucky/wiki_db.py
@@ -1,10 +1,14 @@
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, MetaData
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-from lucky.credentials import WIKI_DB_STRING
+try:
+  from lucky.credentials import WIKI_DB_STRING
+  _engine = create_engine(WIKI_DB_STRING)
+except ImportError:
+  # This won't actually work because the tables are not there
+  _engine = create_engine('sqlite:///:memory:')
 
-Base = declarative_base()
-
-_engine = create_engine(WIKI_DB_STRING)
+metadata = MetaData()
+Base = declarative_base(metadata=metadata)
 Session = sessionmaker(bind=_engine)
