f5fe7d8b5e270d977c17506c5e296b545957480e
Travis Briggs <audiodude@gmail.com>
2018-09-30 23:19:32 +0000

    Allow for empty credentials, so that tests can be run without them. Also assures that no test will hit live services.

--- lucky/lucky/wp10_db.py
+++ lucky/lucky/wp10_db.py
@@ -2,10 +2,13 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-from lucky.credentials import WP10_DB_STRING
+try:
+  from lucky.credentials import WP10_DB_STRING
+  _engine = create_engine(WP10_DB_STRING)
+except ImportError:
+  # This won't actually work because the tables are not there
+  _engine = create_engine('sqlite:///:memory:')
 
 Base = declarative_base()
-
-_engine = create_engine(WP10_DB_STRING)
 Session = sessionmaker(bind=_engine)
 
