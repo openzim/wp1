ae758d2df358064f0a92b32810df2022915ed4cc
Travis Briggs <audiodude@gmail.com>
2018-10-12 20:27:09 +0000

    Add update of global_articles table to update_project function

--- lucky/lucky/logic/project.py
+++ lucky/lucky/logic/project.py
@@ -106,7 +106,7 @@ def update_project_assessments(
     wiki_session, wp10_session, project, extra_assessments, kind)
 
   for rating, category in rating_to_category.items():
-    logging.info('Fetching article list for %s' % category)
+    logging.info('Fetching article list for %r' % category.decode('utf-8'))
     count = 0
     for page in logic_page.get_pages_by_category(wiki_session, category):
       # Talk pages are tagged, we want the NS of the article itself.
@@ -280,6 +280,39 @@ def update_project_record(wp10_session, project, metadata):
 
   wp10_session.add(project)
 
+def update_articles_table(wp10_session, project):
+  # This is a fairly complex query, but it performs relatively well. It would be complex to
+  # translate it to the ORM layer and perserve the nuance of what to do when certain values are
+  # NULL, etc. So we'll just copy it from the old codebase and execute it.
+  wp10_session.execute('''
+    REPLACE INTO lucky_global_articles
+    SELECT art, max(qrating), max(irating), max(score)
+    FROM
+    ( SELECT art, qrating, irating, score
+      FROM
+        (SELECT a_article as art, a_quality as qrating, a_importance as irating, 
+                a_score as score
+           FROM global_articles
+           JOIN ratings 
+               ON r_namespace = 0 AND r_project = :project AND a_article = r_article
+        ) AS tableone
+      UNION
+        (SELECT r_article as art, qual.gr_ranking as qrating, imp.gr_ranking as irating, 
+                r_score as score
+          FROM  ratings
+          JOIN  categories as ci ON r_project = ci.c_project
+            AND ci.c_type = 'importance' AND r_importance = ci.c_rating
+          JOIN  categories as cq ON r_project = cq.c_project
+            AND cq.c_type = 'quality' AND r_quality = cq.c_rating
+          JOIN  global_rankings AS qual ON qual.gr_type = 'quality' 
+                                  AND qual.gr_rating = cq.c_replacement
+          JOIN  global_rankings AS imp  ON imp.gr_type = 'importance' 
+                                     AND imp.gr_rating = ci.c_replacement
+        WHERE r_namespace = 0 and r_project = :project )
+    ) as tabletwo
+    GROUP BY art
+  ''', {'project': project.project})
+
 def update_project(wiki_session, wp10_session, project):
   extra_assessments = api_project.get_extra_assessments(project.project)
   timestamp = project.timestamp
@@ -302,3 +335,5 @@ def update_project(wiki_session, wp10_session, project):
   ## changing, skip it for now.
   # update_project_scores(wp10_session, project)
   # wp10_session.commit()
+
+  update_articles_table(wp10_session, project)
