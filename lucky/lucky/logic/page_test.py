1712818cca09c9c7c9eaffb5f073cf0487a5eccc
Travis Briggs <audiodude@gmail.com>
2019-01-27 15:33:13 -0800

    Fix page_test

--- lucky/lucky/logic/page_test.py
+++ lucky/lucky/logic/page_test.py
@@ -2,44 +2,65 @@ from datetime import datetime
 from unittest.mock import patch
 import time
 
-from lucky.base_orm_test import BaseWikiOrmTest, BaseWpOneOrmTest
+import attr
+
+from lucky.base_db_test import BaseWikiDbTest, BaseWpOneDbTest
 from lucky.constants import TS_FORMAT
 from lucky.logic import page as logic_page
+from lucky.logic import project as logic_project
 from lucky.models.wp10.log import Log
 from lucky.models.wp10.move import Move
 from lucky.models.wp10.namespace import Namespace, NsType
 from lucky.models.wp10.project import Project
 from lucky.models.wiki.page import Page
 
-class LogicPageCategoryTest(BaseWikiOrmTest):
+
+def get_all_moves(wp10db):
+  with wp10db.cursor() as cursor:
+    cursor.execute('SELECT * FROM ' + Move.table_name)
+    return [Move(**db_move) for db_move in cursor.fetchall()]
+
+
+def get_all_logs(wp10db):
+  with wp10db.cursor() as cursor:
+    cursor.execute('SELECT * FROM ' + Log.table_name)
+    return [Log(**db_log) for db_log in cursor.fetchall()]
+
+
+class LogicPageCategoryTest(BaseWikiDbTest):
   def setUp(self):
     super().setUp()
     ts = datetime(2018, 9, 30, 12, 30, 0)
-    self.session.execute('''
-      INSERT INTO page (page_id, page_namespace, page_title)
-      VALUES (:id, :ns, :title)
-    ''', [
-      {'id': 100, 'ns': 0, 'title': b'The cape of Superman'},
-      {'id': 101, 'ns': 0, 'title': b'Powers of Superman'},
-      {'id': 102, 'ns': 14, 'title': b'Places Superman vacations'},
-      {'id': 103, 'ns': 14, 'title': b'Superman Facts'},
-    ])
-    self.session.execute('''
-      INSERT INTO categorylinks (cl_from, cl_to, cl_timestamp)
-      VALUES (:from, :to, :timestamp)
-    ''', [
-      {'from': 100, 'to': b'Articles about Superman', 'timestamp': ts},
-      {'from': 101, 'to': b'Articles about Superman', 'timestamp': ts},
-      {'from': 102, 'to': b'Articles about Superman', 'timestamp': ts},
-      {'from': 103, 'to': b'Articles about Superman', 'timestamp': ts},
-    ])
-    self.session.commit()
+    with self.wikidb.cursor() as cursor:
+      pages = [
+        {'id': 100, 'ns': 0, 'title': b'The cape of Superman'},
+        {'id': 101, 'ns': 0, 'title': b'Powers of Superman'},
+        {'id': 102, 'ns': 14, 'title': b'Places Superman vacations'},
+        {'id': 103, 'ns': 14, 'title': b'Superman Facts'},
+      ]
+      for page in pages:
+        cursor.execute('''
+          INSERT INTO page (page_id, page_namespace, page_title)
+          VALUES (%(id)s, %(ns)s, %(title)s)
+        ''', page)
+      cls = [
+        {'from': 100, 'to': b'Articles about Superman', 'timestamp': ts},
+        {'from': 101, 'to': b'Articles about Superman', 'timestamp': ts},
+        {'from': 102, 'to': b'Articles about Superman', 'timestamp': ts},
+        {'from': 103, 'to': b'Articles about Superman', 'timestamp': ts},
+      ]
+      for cl in cls:
+        cursor.execute('''
+          INSERT INTO categorylinks (cl_from, cl_to, cl_timestamp)
+          VALUES (%(from)s, %(to)s, %(timestamp)s)
+        ''', cl)
+    self.wikidb.commit()
     
   def test_get_category_pages(self):
     titles = set()
     for page in logic_page.get_pages_by_category(
-        self.session, b'Articles about Superman'):
-      titles.add(page.title)
+        self.wikidb, b'Articles about Superman'):
+      titles.add(page.page_title)
 
     self.assertEqual(4, len(titles))
     self.assertTrue(b'The cape of Superman' in titles)
@@ -50,24 +71,17 @@ class LogicPageCategoryTest(BaseWikiOrmTest):
   def test_get_category_pages_ns_filter(self):
     titles = set()
     for page in logic_page.get_pages_by_category(
-        self.session, b'Articles about Superman', ns=14):
-      print(page.namespace)
-      titles.add(page.title)
+        self.wikidb, b'Articles about Superman', ns=14):
+      titles.add(page.page_title)
 
-    print(titles)
     self.assertEqual(2, len(titles))
     self.assertTrue(b'Superman Facts' in titles)
     self.assertTrue(b'Places Superman vacations' in titles)
 
 
-class LogicPageMovesTest(BaseWpOneOrmTest):
+class LogicPageMovesTest(BaseWpOneDbTest):
   def setUp(self):
     super().setUp()
-    n = Namespace(dbname=b'enwiki_p', domain=b'en.wikipedia.org', id=0,
-                  name=b'', type=NsType.primary)
-    self.session.add(n)
-    self.session.commit()
-
     self.timestamp_str = '2011-04-28T12:30:00Z'
     self.expected_ns = 0
     self.expected_title = 'Article_moved_to'
@@ -115,14 +129,14 @@ class LogicPageMovesTest(BaseWpOneOrmTest):
   @patch('lucky.logic.api.page.site')
   def test_no_redirect_no_move(self, unused_patched_site):
     move_data = logic_page.get_move_data(
-      self.session, 0, b'Some Moved Article', datetime(1970, 1, 1))
+      self.wp10db, 0, b'Some Moved Article', datetime(1970, 1, 1))
     self.assertIsNone(move_data)
 
   @patch('lucky.logic.api.page.site')
   def test_get_redirect_from_api(self, patched_site):
     patched_site.api.side_effect = lambda *args, **kwargs: self.api_return
     move_data = logic_page.get_move_data(
-      self.session, 0, b'Some Moved Article', datetime(1970, 1, 1))
+      self.wp10db, 0, b'Some Moved Article', datetime(1970, 1, 1))
 
     self.assertIsNotNone(move_data)
     self.assertEqual(self.expected_ns, move_data['dest_ns'])
@@ -134,7 +148,7 @@ class LogicPageMovesTest(BaseWpOneOrmTest):
   def test_get_single_move_from_api(self, patched_site):
     patched_site.logevents.side_effect = lambda *args, **kwargs: self.le_return
     move_data = logic_page.get_move_data(
-      self.session, 0, b'Some Moved Article', datetime(1970, 1, 1))
+      self.wp10db, 0, b'Some Moved Article', datetime(1970, 1, 1))
 
     self.assertIsNotNone(move_data)
     self.assertEqual(self.expected_ns, move_data['dest_ns'])
@@ -146,7 +160,7 @@ class LogicPageMovesTest(BaseWpOneOrmTest):
   def test_get_most_recent_move_from_api(self, patched_site):
     patched_site.logevents.side_effect = lambda *args, **kwargs: self.le_multi
     move_data = logic_page.get_move_data(
-      self.session, 0, b'Some Moved Article', datetime(1970, 1, 1))
+      self.wp10db, 0, b'Some Moved Article', datetime(1970, 1, 1))
 
     self.assertIsNotNone(move_data)
     self.assertEqual(self.expected_ns, move_data['dest_ns'])
@@ -158,23 +172,23 @@ class LogicPageMovesTest(BaseWpOneOrmTest):
   def test_get_redirect_too_old_from_api(self, patched_site):
     patched_site.api.side_effect = lambda *args, **kwargs: self.api_return
     move_data = logic_page.get_move_data(
-      self.session, 0, b'Some Moved Article', datetime(2014, 1, 1))
+      self.wp10db, 0, b'Some Moved Article', datetime(2014, 1, 1))
     self.assertIsNone(move_data)
     
   @patch('lucky.logic.api.page.site')
   def test_get_single_move_too_old_from_api(self, patched_site):
     patched_site.logevents.side_effect = lambda *args, **kwargs: self.le_return
     move_data = logic_page.get_move_data(
-      self.session, 0, b'Some Moved Article', datetime(2014, 1, 1))
+      self.wp10db, 0, b'Some Moved Article', datetime(2014, 1, 1))
     self.assertIsNone(move_data)
 
-class LogicPageMoveDbTest(BaseWpOneOrmTest):
+
+class LogicPageMoveDbTest(BaseWpOneDbTest):
   def setUp(self):
     super().setUp()
-    self.project = Project(project=b'Testing')
-    self.session.add(self.project)
-    self.session.commit()
-
+    self.project = Project(p_project=b'Testing', p_timestamp='201001010000')
+    logic_project.insert_or_update(self.wp10db, self.project)
+    
     self.old_ns = 0
     self.old_article = b'The history of testing'
     self.new_ns = 0
@@ -185,61 +199,61 @@ class LogicPageMoveDbTest(BaseWpOneOrmTest):
   
   def test_new_move(self):
     logic_page.update_page_moved(
-      self.session, self.project, self.old_ns, self.old_article,
+      self.wp10db, self.project, self.old_ns, self.old_article,
       self.new_ns, self.new_article, self.dt)
-    self.session.commit()
 
-    move = self.session.query(Move).filter(
-      Move.old_article == self.old_article).first()
+    with self.wp10db.cursor() as cursor:
+      cursor.execute('SELECT * FROM ' + Move.table_name + '''
+        WHERE m_old_article = %(old_article)s
+      ''', {'old_article': self.old_article})
+      move = Move(**cursor.fetchone())
 
     self.assertIsNotNone(move)
-    self.assertEqual(self.old_ns, move.old_namespace)
-    self.assertEqual(self.old_article, move.old_article)
-    self.assertEqual(self.new_ns, move.new_namespace)
-    self.assertEqual(self.new_article, move.new_article)
-    self.assertEqual(self.timestamp_db, move.timestamp)
+    self.assertEqual(self.old_ns, move.m_old_namespace)
+    self.assertEqual(self.old_article, move.m_old_article)
+    self.assertEqual(self.new_ns, move.m_new_namespace)
+    self.assertEqual(self.new_article, move.m_new_article)
+    self.assertEqual(self.timestamp_db, move.m_timestamp)
 
   def test_new_move_log(self):
     logic_page.update_page_moved(
-      self.session, self.project, self.old_ns, self.old_article,
+      self.wp10db, self.project, self.old_ns, self.old_article,
       self.new_ns, self.new_article, self.dt)
-    self.session.commit()
 
-    log = self.session.query(Log).filter(
-      Log.article == self.old_article).first()
+    with self.wp10db.cursor() as cursor:
+      cursor.execute('SELECT * FROM ' + Log.table_name + '''
+        WHERE l_article = %(old_article)s
+      ''', {'old_article': self.old_article})
+      log = Log(**cursor.fetchone())
 
     self.assertIsNotNone(log)
-    self.assertEqual(self.old_ns, log.namespace)
-    self.assertEqual(self.old_article, log.article)
-    self.assertEqual(b'moved', log.action)
-    self.assertEqual(b'', log.old)
-    self.assertEqual(b'', log.new)
-    self.assertEqual(self.timestamp_db, log.revision_timestamp)
+    self.assertEqual(self.old_ns, log.l_namespace)
+    self.assertEqual(self.old_article, log.l_article)
+    self.assertEqual(b'moved', log.l_action)
+    self.assertEqual(b'', log.l_old)
+    self.assertEqual(b'', log.l_new)
+    self.assertEqual(self.timestamp_db, log.l_revision_timestamp)
 
   def test_does_not_add_existing_move(self):
     logic_page.update_page_moved(
-      self.session, self.project, self.old_ns, self.old_article,
+      self.wp10db, self.project, self.old_ns, self.old_article,
       self.new_ns, self.new_article, self.dt)
-    self.session.commit()
 
     logic_page.update_page_moved(
-      self.session, self.project, self.old_ns, self.old_article,
+      self.wp10db, self.project, self.old_ns, self.old_article,
       self.new_ns, self.new_article, self.dt)
-    self.session.commit()
 
-    all_moves = list(self.session.query(Move).all())
+    all_moves = get_all_moves(self.wp10db)
     self.assertEqual(1, len(all_moves))
 
   def test_does_not_add_existing_log(self):
     logic_page.update_page_moved(
-      self.session, self.project, self.old_ns, self.old_article,
+      self.wp10db, self.project, self.old_ns, self.old_article,
       self.new_ns, self.new_article, self.dt)
-    self.session.commit()
 
     logic_page.update_page_moved(
-      self.session, self.project, self.old_ns, self.old_article,
+      self.wp10db, self.project, self.old_ns, self.old_article,
       self.new_ns, self.new_article, self.dt)
-    self.session.commit()
 
-    all_moves = list(self.session.query(Log).all())
-    self.assertEqual(1, len(all_moves))
+    all_logs = get_all_logs(self.wp10db)
+    self.assertEqual(1, len(all_logs))
