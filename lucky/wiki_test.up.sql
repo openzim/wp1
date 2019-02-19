dbc179be4a30959a21e3c42e9179b0706e4bccfc
Travis Briggs <audiodude@gmail.com>
2019-02-18 19:49:54 -0800

    Update and fix tests for new moves API

--- lucky/wiki_test.up.sql
+++ lucky/wiki_test.up.sql
@@ -1,7 +1,8 @@
 CREATE TABLE `page` (
   `page_id` int(8) unsigned NOT NULL,
   `page_namespace` int(11) NOT NULL,
-  `page_title` varbinary(255) NOT NULL
+  `page_title` varbinary(255) NOT NULL,
+  `page_touched` varbinary(14) NOT NULL DEFAULT ''
 );
 
 CREATE TABLE `categorylinks` (
@@ -9,4 +10,12 @@ CREATE TABLE `categorylinks` (
   `cl_to` varbinary(255) NOT NULL DEFAULT '',
   `cl_sortkey` varbinary(230) NOT NULL DEFAULT '',
   `cl_timestamp` timestamp
+);
+
+CREATE TABLE `redirect` (
+  `rd_from` int(8) unsigned NOT NULL DEFAULT 0,
+  `rd_namespace` int(11) NOT NULL DEFAULT 0,
+  `rd_title` varbinary(255) NOT NULL DEFAULT '',
+  `rd_interwiki` varbinarY(32) DEFAULT NULL,
+  `rd_fragment` varbinary(255) DEFAULT NULL
 );
