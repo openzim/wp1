dfaca8f888e506c549b7557f97b4ced537fff050
Kelson <kelson@kiwix.org>
2019-02-19 03:07:15 +0000

    Make conf loading work no matter the cwd

--- lucky/lucky/conf.py
+++ lucky/lucky/conf.py
@@ -1,7 +1,12 @@
+import os
+
 import json
 
+def _get_conf_path():
+  return os.path.join(os.path.dirname(os.path.dirname(__file__)), 'conf.json')
+
 _conf = None
-with open('conf.json') as f:
+with open(_get_conf_path()) as f:
   _conf = json.load(f)
 
 def get_conf():
