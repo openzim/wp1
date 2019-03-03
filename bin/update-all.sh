61e3b18c5f2726eb604edb29be864512de631f9a
Travis Briggs <audiodude@gmail.com>
2019-03-03 00:47:07 +0000

    Update the update-all script to use the 22 beta projects in the new bot and exclude them from the old bot

--- bin/update-all.sh
+++ bin/update-all.sh
@@ -20,11 +20,11 @@ time ./download.pl --releases
 echo "Reviews"
 time ./download.pl --reviews
 
-echo "Update Catholicism project with new bot"
-../lucky/venv/bin/python3 ../lucky/update_catholicism.py
+echo "Update 22 beta projects with new bot"
+../lucky/venv/bin/python3 ../lucky/update.py --includefile=../project_list_22.txt
 
-echo "Update all projects, except Catholicism"
-time ./download.pl --all --exclude=Catholicism
+echo "Update all projects, except 22 beta projects"
+time ./download.pl --all --excludefile ../project_list_22.txt
 
 echo "Global table"
 time ./mktable.pl --purge --mode global
