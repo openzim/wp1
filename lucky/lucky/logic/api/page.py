353a3051685496c10a89ec63209f601bbbff0257
Travis Briggs <audiodude@gmail.com>
2019-06-20 15:02:17 +0000

    Retry API requests that error out

--- lucky/lucky/logic/api/page.py
+++ lucky/lucky/logic/api/page.py
@@ -10,8 +10,20 @@ logger = logging.getLogger(__name__)
 
 def get_redirect(title_with_ns):
   logger.debug('Querying api for redirects for %s', title_with_ns)
-  res = site.api(
-    'query', titles=title_with_ns, redirects=1, prop='revisions', rvlimit=1)
+
+  res = None
+  retries = 3
+  while retries:
+    try:
+      res = site.api('query', titles=title_with_ns, redirects=1,
+                     prop='revisions', rvlimit=1)
+      break
+    except:
+      retries -= 1
+
+  if res is None:
+    logger.warn('Error contacting API, returning None')
+    return None
 
   if 'query' not in res or 'redirects' not in res['query']:
     return None
@@ -30,16 +42,37 @@ def get_redirect(title_with_ns):
 
 def get_moves(title_with_ns):
   logger.debug('Querying api for moves of page %s', title_with_ns)
-  res = site.logevents(title=title_with_ns, type='move')
+
+  res = None
+  retries = 3
+  while retries:
+    try:
+      res = site.logevents(title=title_with_ns, type='move')
+      break
+    except:
+      retries -= 1
+
+  if res is None:
+    logger.warn('Error contacting API, returning None')
+    return None
 
   ans = []
-  for event in res:
-    if 'params' not in event:
-      continue
-    ans.append({
-      'ns': event['params']['target_ns'],
-      'title': event['params']['target_title'].replace(' ', '_'),
-      'timestamp_dt': datetime.fromtimestamp(time.mktime(event['timestamp'])),
-    })
+  retries = 3
+  while retries:
+    try:
+      for event in res:
+        if 'params' not in event:
+          continue
+        ans.append({
+          'ns': event['params']['target_ns'],
+          'title': event['params']['target_title'].replace(' ', '_'),
+          'timestamp_dt': datetime.fromtimestamp(time.mktime(event['timestamp'])),
+        })
+      break
+    except:
+      retries -= 1
+
+  if retries == 0:
+    logger.warn('Error contacting continuation API, returning None')
 
   return ans or None
