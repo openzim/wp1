9004da93c706af4d97ccf542e27e5afb5d490928
Travis Briggs <audiodude@gmail.com>
2019-07-14 23:05:48 +0000

    Update the update-all script to use the 212 full beta projects in the new bot and exclude them from the old bot

--- bin/update-all.sh
+++ bin/update-all.sh
@@ -22,11 +22,11 @@ time ./download.pl --releases
 echo "Reviews"
 time ./download.pl --reviews
 
-# echo "Update all projects with new bot"
-# ../lucky/venv/bin/python3 ../lucky/update.py --all
+echo "Update 212 projects with new bot"
+../lucky/venv/bin/python3 ../lucky/update.py --all --includefile=../project_list_212.txt
 
-echo "Update all projects with old bot, excluding Years"
-time ./download.pl --all --exclude Years
+echo "Update all projects with old bot, except the 212"
+time ./download.pl --all --excludefile ../project_list_212.txt
 
 echo "Global table"
 time ./mktable.pl --purge --mode global
