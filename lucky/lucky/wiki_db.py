faaeec13309d05254d03680f6dc6cd1cc8ce1f50
Travis Briggs <audiodude@gmail.com>
2019-08-11 10:18:43 +0200

    Update database connections to use credentials file again

--- lucky/lucky/wiki_db.py
+++ lucky/lucky/wiki_db.py
@@ -3,11 +3,18 @@ import os
 import pymysql
 import pymysql.cursors
 
+try:
+  from credentials import WIKI_CREDS
+except ImportError:
+  raise ImportError('Could not find credentials module. Have you populated it '
+                    'based on credentials.py.example?')
+
 def connect():
-  return pymysql.connect(
-    db='enwiki_p',
-    host="enwiki.analytics.db.svc.eqiad.wmflabs",
-    read_default_file=os.path.expanduser("~/replica.my.cnf"),
-    charset=None,
-    use_unicode=False,
-    cursorclass=pymysql.cursors.SSDictCursor)
+  kwargs = {
+    'charset': None,
+    'use_unicode': False,
+    'cursorclass': pymysql.cursors.SSDictCursor,
+    **WIKI_CREDS
+  }
+
+  return pymysql.connect(**kwargs)
