951edeb4ffaeeb85b295481c8a50056e8350280b
Travis Briggs <audiodude@gmail.com>
2019-08-11 10:21:08 +0200

    Update import with proper path

--- lucky/lucky/wiki_db.py
+++ lucky/lucky/wiki_db.py
@@ -4,7 +4,7 @@ import pymysql
 import pymysql.cursors
 
 try:
-  from credentials import WIKI_CREDS
+  from lucky.credentials import WIKI_CREDS
 except ImportError:
   raise ImportError('Could not find credentials module. Have you populated it '
                     'based on credentials.py.example?')
