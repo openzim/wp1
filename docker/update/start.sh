be7f12d3bca1e6a0c2b692b25d9bf8a6b6c40567
Emmanuel Engelhart <kelson@kiwix.org>
2019-08-12 12:11:08 +0200

    Introduce Cron with scheduled update jobs

---
+++ docker/update/start.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+# Update Cron
+echo "Install Cron to daily schedule update jobs"
+{ \
+  echo "#!/bin/sh" ; \
+  echo "/usr/bin/flock -w 0 /dev/shm/cron.lock python /usr/src/app/enqueue-all.py >>/dev/shm/schedule_updates.log 2>&1" ; \
+} > /etc/cron.daily/schedule_updates && chmod +x /etc/cron.daily/schedule_updates
+
+# Run the workers
+# TODO
