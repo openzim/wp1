47b8feb080979d98ed71422d183eb2dd247913f9
Travis Briggs <audiodude@gmail.com>
2019-08-14 15:32:02 +0200

    Add script for enquueuing the global table upload, and add it to cron

--- docker/workers/start.sh
+++ docker/workers/start.sh
@@ -4,7 +4,8 @@
 echo "Install Cron to daily schedule update & upload tasks"
 { \
   echo "#!/bin/sh" ; \
-  echo "/usr/bin/flock -w 0 /dev/shm/cron.lock python /usr/src/app/enqueue-all.py >>/dev/shm/schedule_tasks.log 2>&1" ; \
+  echo "/usr/bin/flock -w 0 /dev/shm/cron.lock python /usr/src/app/enqueue-all.py >> /dev/shm/schedule_tasks.log 2>&1" ; \
+  echo "/usr/bin/flock -w 0 /dev/shm/cron.global.lock python /usr/src/app/enqueue-global.py >> /dev/shm/global_table.log 2>&1" ; \
 } > /etc/cron.daily/schedule_tasks && chmod +x /etc/cron.daily/schedule_tasks
 
 # Run the workers
