ab447b4488c0470688cb370803d19158eb543cbd
Emmanuel Engelhart <kelson@kiwix.org>
2019-08-14 20:46:44 +0200

    Remove old Perl code

--- maintenance/a.sql
+++
@@ -1,8 +0,0 @@
-select dest.page_title from page as dest
- join pagelinks on pl_namespace = dest.page_namespace
-                  and pl_title = dest.page_title
- join page as source on pl_from = source.page_title
- where source.page_namespace = 0 
-   and source.page_title = 'List_of_decades'
-   and dest.page_namespace = 0;
-
