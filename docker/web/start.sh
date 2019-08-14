d23af02a7f26cbd6e100e941c95d8f4243475d8d
Travis Briggs <audiodude@gmail.com>
2019-08-14 22:17:35 +0200

    Update for lint found by codecoverage

--- docker/web/start.sh
+++ docker/web/start.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-USER=`head -n 1 app/rq-credentials.txt`
-PASS=`tail -n 1 app/rq-credentials.txt`
+USER=$(head -n 1 app/rq-credentials.txt)
+PASS=$(tail -n 1 app/rq-credentials.txt)
 
 rq-dashboard --username $USER --password $PASS -H redis
