9f961e007eaad0a36e1f8d94d09f020e171b8a4c
Emmanuel Engelhart <kelson@kiwix.org>
2019-08-14 20:46:44 +0200

    Remove old Perl code

--- maintenance/Dates.sql
+++
@@ -1,3 +0,0 @@
-select page_title
-  from page join categorylinks on page_id = cl_from
-  where page_namespace = 0 and cl_to = "Days_of_the_year"
--- maintenance/Years.sql
+++
@@ -1,8 +0,0 @@
-select pl_title
-  from pagelinks join page as src on pl_from = src.page_id
-  join page as dst on pl_namespace = dst.page_namespace
-              and pl_title = dst.page_title
- where src.page_namespace = 0 
-   and src.page_title = 'List_of_years'
-   and dst.page_namespace = 0;
-
