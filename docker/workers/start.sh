db8f425a36e1fc3bfb585188e26aabc4add59a7b
Emmanuel Engelhart <kelson@kiwix.org>
2019-08-15 16:09:59 +0200

    Better Crontab entries

--- docker/workers/start.sh
+++ docker/workers/start.sh
@@ -1,12 +1,8 @@
 #!/bin/sh
 
-# Cron
-echo "Install Cron to daily schedule update & upload tasks"
-{ \
-  echo "#!/bin/sh" ; \
-  echo "/usr/bin/flock -w 0 /dev/shm/cron.lock python /usr/src/app/enqueue-all.py >> /dev/shm/schedule_tasks.log 2>&1" ; \
-  echo "/usr/bin/flock -w 0 /dev/shm/cron.global.lock python /usr/src/app/enqueue-global.py >> /dev/shm/global_table.log 2>&1" ; \
-} > /etc/cron.daily/schedule_tasks && chmod +x /etc/cron.daily/schedule_tasks
+# Run cron
+echo "Starting crond in the background"
+cron
 
 # Run the workers
 echo "Starting supervisord in the foreground"
