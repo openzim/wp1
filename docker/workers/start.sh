a913268d7281f8b0ef6e6cd450dd0d9da2ecea11
Emmanuel Engelhart <kelson@kiwix.org>
2019-08-15 16:29:32 +0200

    Remove start.sh

--- docker/workers/start.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-# Run cron
-echo "Starting crond in the background"
-cron
-
-# Run the workers
-echo "Starting supervisord in the foreground"
-supervisord -c /usr/src/app/supervisord.conf -n
