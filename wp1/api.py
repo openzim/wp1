fd9c40e6464e76d02775fc3e841c715a0770e27e
Travis Briggs <audiodude@gmail.com>
2019-08-20 12:10:00 -0700

    Set up api methods so they can be tested without messing up other tests

--- wp1/api.py
+++ wp1/api.py
@@ -5,18 +5,28 @@ import mwclient
 logger = logging.getLogger(__name__)
 _ua = 'WP1.0Bot/3.0. Run by User:Audiodude. Using mwclient/0.9.1'
 
+def get_credentials():
+  try:
+    from wp1.credentials import API_CREDS
+    return API_CREDS
+  except ImportError:
+    # No credentials, probably in development environment.
+    pass
+
 site = None
 def login():
   global site
   try:
-    from wp1.credentials import API_CREDS
+    api_creds = get_credentials()
+    print(repr(api_creds))
+    if api_creds is None:
+      logger.warning('Not creating site, no credentials')
+      return
     site = mwclient.Site('en.wikipedia.org', clients_useragent=_ua)
-    site.login(API_CREDS['user'], API_CREDS['pass'])
+    print(repr(site))
+    site.login(api_creds['user'], api_creds['pass'])
   except mwclient.errors.LoginError:
     logger.exception('Exception logging into wikipedia')
-  except ImportError:
-    # No credentials, probably in development environment.
-    pass
 
 
 # Global login on startup.
--- wp1/api_test.py
+++ wp1/api_test.py
@@ -1,46 +1,53 @@
+import importlib
 import sys
 import unittest
 from unittest.mock import MagicMock, patch
 
 import mwclient
 
-from wp1 import api
+import wp1.api
+
+class ApiWithCredsTest(unittest.TestCase):
+  @patch('wp1.api.get_credentials')
+  @patch('wp1.api.mwclient.Site')
+  def test_login(self, patched_mwsite, patched_credentials):
+    site = patched_mwsite()
+    wp1.api.login()
+    self.assertEqual(1, site.login.call_count)
+
+  @patch('wp1.api.get_credentials')
+  @patch('wp1.api.mwclient.Site')
+  @patch('wp1.api.logger')
+  def test_login_exception(self, patched_logger, patched_mwsite, patched_credentials):
+    site = patched_mwsite()
+    print('in test', site)
+    site.login.side_effect = mwclient.errors.LoginError()
+    wp1.api.login()
+    self.assertEqual(1, patched_logger.exception.call_count)
+
 
 class ApiTest(unittest.TestCase):
   def setUp(self):
     self.page = MagicMock()
+    self.original_save_page = wp1.api.save_page
 
-  def test_login_called_when_import_creds(self):
-    api.site = MagicMock()
-    mock_credentials = MagicMock()
-    sys.modules['wp1.credentials'] = mock_credentials
-    api.login()
-    self.assertEqual(1, api.site.login.call_count)
-
-  def test_login_exception_when_import_creds(self):
-    api.logger = MagicMock()
-    api.site = MagicMock()
-    mock_credentials = MagicMock()
-    api.site.login.side_effect = mwclient.errors.LoginError()
-    api.login()
-    self.assertEqual(1, api.logger.exception.call_count)
-
-  def test_save_page(self):
-    api.site = MagicMock()
-    api.save_page(self.page, '<code>', 'edit summary')
-    self.assertEqual(1, len(self.page.save.call_args_list))
+  @patch('wp1.api.site')
+  def test_save_page(self, patched_site):
+    wp1.api.save_page(self.page, '<code>', 'edit summary')
+    self.assertEqual(1, self.page.save.call_count)
     self.assertEqual(('<code>', 'edit summary'), self.page.save.call_args[0])
 
+  @patch('wp1.api.site')
   @patch('wp1.api.login')
-  def test_save_page_tries_login_on_exception(self, patched_login):
-    api.site = MagicMock()
+  def test_save_page_tries_login_on_exception(self, patched_login, patched_site):
     self.page.save.side_effect = mwclient.errors.AssertUserFailedError()
     with self.assertRaises(mwclient.errors.AssertUserFailedError):
-      actual = api.save_page(self.page, '<code>', 'edit summary')
+      actual = wp1.api.save_page(self.page, '<code>', 'edit summary')
       self.assertTrue(actual)
       self.assertEqual(1, len(patched_login.call_args_list))
 
-  def test_save_page_skips_login_on_none_site(self):
-    api.site = None
-    actual = api.save_page(self.page, '<code>', 'edit summary')
+  @patch('wp1.api')
+  def test_save_page_skips_login_on_none_site(self, patched_api):
+    patched_api.site = None
+    actual = self.original_save_page(self.page, '<code>', 'edit summary')
     self.assertFalse(actual)
