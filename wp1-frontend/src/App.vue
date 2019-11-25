c72ecd9a6f0665ec12188245caef8a18ddc8d2cc
Travis Briggs <audiodude@gmail.com>
2019-11-24 19:38:43 -0800

    Updated with a static "stats" component which displays the number of projects

--- wp1-frontend/src/App.vue
+++ wp1-frontend/src/App.vue
@@ -1,13 +1,18 @@
 <template>
   <div id="app">
-    Empty Hello world. Hello!
+    <h1>Wikipedia 1.0 Server</h1>
+    <Stats></Stats>
   </div>
 </template>
 
 <script>
+import Stats from './components/Stats.vue'
 
 export default {
   name: 'app',
+  components: {
+    Stats,
+  },
 }
 </script>
 
---
+++ wp1-frontend/src/components/Stats.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    There are currently {{numProjects}} projects being tracked and updated each
+    day.
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'stats',
+  data: function() {
+    return {
+      'numProjects': 2497
+    }
+  },
+}
+</script>
+
+<style scoped>
+
+</style>
