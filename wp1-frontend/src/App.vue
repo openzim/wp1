421ecc1986d460db91fa7c9c98c29262d92e0c89
Travis Briggs <audiodude@gmail.com>
2019-11-29 22:09:10 -0800

    Add Autocomplete component which fetches a list of projects and allows the user to search for projects within that list

--- wp1-frontend/src/App.vue
+++ wp1-frontend/src/App.vue
@@ -6,22 +6,33 @@
         <Stats></Stats>
       </div>
     </div>
+    <div class="row">
+      <div class="col-6">
+        <p>
+          A list of indexed projects is available using the autocomplete search
+          box below
+        </p>
+        <Autocomplete></Autocomplete>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
+import Autocomplete from './components/Autocomplete.vue'
 import Stats from './components/Stats.vue'
 
 export default {
   name: 'app',
   components: {
+    Autocomplete,
     Stats,
   },
 }
 </script>
 
 <style>
-  #app {
+  .row {
     margin-top: 10px;
   }
 </style>
