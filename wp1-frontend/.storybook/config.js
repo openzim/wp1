b76df27ed11a8a1bc61e0e85b038e756484f68bd
Travis Briggs <audiodude@gmail.com>
2019-11-30 12:24:50 -0800

    Remove fake stories and add first real story

--- wp1-frontend/.storybook/config.js
+++ wp1-frontend/.storybook/config.js
@@ -1,4 +1,7 @@
 import { configure } from '@storybook/vue';
 
+// Bootstrap is used for all component styles.
+import 'bootstrap/dist/css/bootstrap.css';
+
 // automatically import all files ending in *.stories.js
 configure(require.context('../stories', true, /\.stories\.js$/), module);
