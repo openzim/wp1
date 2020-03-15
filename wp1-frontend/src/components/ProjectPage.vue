ce01951c51148ab649c60104a67224ae90f62fa8
Travis Briggs <audiodude@gmail.com>
2020-03-15 12:39:39 -0700

    Basic routing, but it does not do anything useful

---
+++ wp1-frontend/src/components/ProjectPage.vue
@@ -0,0 +1,48 @@
+<template>
+  <div>
+    <div class="row">
+      <div class="col-6">
+        <p>
+          A list of indexed projects is available using the autocomplete search
+          box below
+        </p>
+        <Autocomplete
+          v-on:select-project="currentProject = $event"
+        ></Autocomplete>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col">
+        <ProjectTable v-bind:projectId="currentProjectId"></ProjectTable>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import Autocomplete from './Autocomplete.vue';
+import ProjectTable from './ProjectTable.vue';
+
+export default {
+  name: 'projectpage',
+  components: {
+    Autocomplete,
+    ProjectTable
+  },
+  data: function() {
+    return {
+      currentProject: null
+    };
+  },
+  computed: {
+    currentProjectId: function() {
+      if (!this.currentProject) {
+        return null;
+      }
+      return this.currentProject.replace(/ /g, '_');
+    }
+  }
+};
+</script>
+
+<style></style>
