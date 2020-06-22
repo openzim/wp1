40593e13f8e90ada63be3564207f2d778e50c967
Travis Briggs <audiodude@gmail.com>
2020-06-22 13:27:37 -0700

    Our ever-escalating coverage war with ourselves

--- wp1/web/articles_test.py
+++ wp1/web/articles_test.py
@@ -19,3 +19,13 @@ class ArticlesTest(BaseWebTestcase):
       expected_url = ('%sindex.php?title=%s&oldid=%s' %
                       (FRONTEND_WIKI_BASE, 'Web%20development', 5555))
       self.assertEqual(expected_url, rv.headers['Location'])
+
+  @patch('wp1.web.articles.get_page')
+  @patch('wp1.web.articles.get_revision_id_by_timestamp')
+  def test_404(self, patched_get_revision, patched_get_page):
+    patched_get_revision.return_value = None
+
+    with self.override_db(self.app), self.app.test_client() as client:
+      rv = client.get(
+          '/v1/articles/Web%20development/2020-01-13T10:00:00Z/redirect')
+      self.assertEqual('404 NOT FOUND', rv.status)
