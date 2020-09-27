d3d3bc97343c2cc6543b41a2bd32759604241036
Travis Briggs <audiodude@gmail.com>
2020-09-27 13:00:16 -0700

    Wire up URL changing and bookmarking ability for Compare pages

--- wp1-frontend/src/components/ComparePage.vue
+++ wp1-frontend/src/components/ComparePage.vue
@@ -9,7 +9,7 @@
     <div class="row mt-4">
       <div class="col-xl-6">
         <Autocomplete
-          :incomingSearch="incomingSearchA || $route.params.projectNameA"
+          :incomingSearch="incomingSearchA"
           :hideInstructions="true"
           v-on:select-project="projectA = $event"
         ></Autocomplete>
@@ -20,6 +20,8 @@
         <ArticleTableRatingSelect
           :projectId="projectIdA"
           :layout="'alternate'"
+          :initialQuality="this.$route.query.quality"
+          :initialImportance="this.$route.query.importance"
           v-on:rating-select="onProjectARatingSelect($event)"
         ></ArticleTableRatingSelect>
       </div>
@@ -27,7 +29,7 @@
     <div class="row mt-4">
       <div class="col-xl-6">
         <Autocomplete
-          :incomingSearch="incomingSearchB || $route.params.projectNameB"
+          :incomingSearch="incomingSearchB"
           :hideInstructions="true"
           v-on:select-project="projectB = $event"
         ></Autocomplete>
@@ -38,11 +40,13 @@
         <ArticleTableRatingSelect
           :projectId="projectIdB"
           :layout="'alternate'"
+          :initialQuality="this.$route.query.qualityB"
+          :initialImportance="this.$route.query.importanceB"
           v-on:rating-select="onProjectBRatingSelect($event)"
         ></ArticleTableRatingSelect>
       </div>
     </div>
-    <div class="row mt-4">
+    <div v-if="showCompareButton" class="row mt-4">
       <div class="col-xl-6">
         <div class="input-group">
           <button
@@ -55,7 +59,7 @@
         </div>
       </div>
     </div>
-    <div v-if="projectsSelected && compareClicked" class="row mt-4">
+    <div v-if="projectsSelected && !showCompareButton" class="row mt-4">
       <div class="col">
         <ArticleTable
           :hideRatingSelect="true"
@@ -68,6 +72,9 @@
           :page="$route.query.page"
           :numRows="$route.query.numRows"
           :articlePattern="$route.query.articlePattern"
+          v-on:page-select="onPageSelect($event)"
+          v-on:name-filter="onNameFilter($event)"
+          v-on:update-page="onUpdatePage($event)"
         ></ArticleTable>
       </div>
     </div>
@@ -99,6 +106,11 @@ export default {
     };
   },
   computed: {
+    showCompareButton: function() {
+      return (
+        !this.compareClicked && (!this.incomingSearchA || !this.incomingSearchB)
+      );
+    },
     projectIdA: function() {
       if (!this.projectA) {
         return null;
@@ -115,9 +127,94 @@ export default {
       return !!this.projectA && !!this.projectB;
     }
   },
+  watch: {
+    $route: function(to) {
+      window.console.log(to.path);
+      if (to.path == '/compare') {
+        this.reset();
+      }
+    },
+    projectAQuality: function(quality) {
+      if (this.showCompareButton) {
+        return;
+      }
+      this.$router.push({
+        path: `/compare/${this.projectA}/${this.projectB}`,
+        query: {
+          quality,
+          importance: this.$route.query.importance,
+          qualityB: this.$route.query.qualityB,
+          importanceB: this.$route.query.importanceB,
+          page: this.$route.query.page,
+          numRows: this.$route.query.numRows,
+          articlePattern: this.$route.query.articlePattern
+        }
+      });
+    },
+    projectAImportance: function(importance) {
+      if (this.showCompareButton) {
+        return;
+      }
+      this.$router.push({
+        path: `/compare/${this.projectA}/${this.projectB}`,
+        query: {
+          quality: this.$route.query.quality,
+          importance,
+          qualityB: this.$route.query.qualityB,
+          importanceB: this.$route.query.importanceB,
+          page: this.$route.query.page,
+          numRows: this.$route.query.numRows,
+          articlePattern: this.$route.query.articlePattern
+        }
+      });
+    },
+    projectBQuality: function(qualityB) {
+      if (this.showCompareButton) {
+        return;
+      }
+      this.$router.push({
+        path: `/compare/${this.projectA}/${this.projectB}`,
+        query: {
+          quality: this.$route.query.quality,
+          importance: this.$route.query.importance,
+          qualityB,
+          importanceB: this.$route.query.importanceB,
+          page: this.$route.query.page,
+          numRows: this.$route.query.numRows,
+          articlePattern: this.$route.query.articlePattern
+        }
+      });
+    },
+    projectBImportance: function(importanceB) {
+      if (this.showCompareButton) {
+        return;
+      }
+      this.$router.push({
+        path: `/compare/${this.projectA}/${this.projectB}`,
+        query: {
+          quality: this.$route.query.quality,
+          importance: this.$route.query.importance,
+          qualityB: this.$route.query.qualityB,
+          importanceB,
+          page: this.$route.query.page,
+          numRows: this.$route.query.numRows,
+          articlePattern: this.$route.query.articlePattern
+        }
+      });
+    }
+  },
   methods: {
     onCompareClick: async function() {
       this.compareClicked = true;
+      this.$router.push({
+        path: `/compare/${this.projectA}/${this.projectB}`,
+        query: {
+          quality: this.projectAQuality,
+          importance: this.projectAImportance,
+          qualityB: this.projectBQuality,
+          importanceB: this.projectBImportance
+        }
+      });
     },
     onProjectARatingSelect: function(event) {
       this.projectAQuality = event.quality;
@@ -126,6 +223,59 @@ export default {
     onProjectBRatingSelect: function(event) {
       this.projectBQuality = event.quality;
       this.projectBImportance = event.importance;
+    },
+    onPageSelect: function(selection) {
+      this.$router.push({
+        path: `/compare/${this.projectA}/${this.projectB}`,
+        query: {
+          quality: this.$route.query.quality,
+          importance: this.$route.query.importance,
+          qualityB: this.$route.query.qualityB,
+          importanceB: this.$route.query.importanceB,
+          page: selection.page,
+          numRows: selection.rows,
+          articlePattern: this.$route.query.articlePattern
+        }
+      });
+    },
+    onNameFilter: function(selection) {
+      this.$router.push({
+        path: `/compare/${this.projectA}/${this.projectB}`,
+        query: {
+          quality: this.$route.query.quality,
+          importance: this.$route.query.importance,
+          qualityB: this.$route.query.qualityB,
+          importanceB: this.$route.query.importanceB,
+          page: this.$route.query.page,
+          numRows: this.$route.query.numRows,
+          articlePattern: selection
+        }
+      });
+    },
+    onUpdatePage: function(page) {
+      this.$router.push({
+        path: `/compare/${this.projectA}/${this.projectB}`,
+        query: {
+          quality: this.$route.query.quality,
+          importance: this.$route.query.importance,
+          qualityB: this.$route.query.qualityB,
+          importanceB: this.$route.query.importanceB,
+          page: page.toString(),
+          numRows: this.$route.query.numRows,
+          articlePattern: this.$route.query.articlePattern
+        }
+      });
+    },
+    reset: function() {
+      this.incomingSearchA = null;
+      this.incomingSearchB = null;
+      this.projectA = null;
+      this.projectB = null;
+      this.projectAQuality = null;
+      this.projectAImportance = null;
+      this.projectBQuality = null;
+      this.projectBImportance = null;
+      this.compareClicked = false;
     }
   }
 };
