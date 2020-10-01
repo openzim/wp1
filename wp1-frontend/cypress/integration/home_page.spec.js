fee23f8234efd5ef023abc2a18f5a727b2874a23
Travis Briggs <audiodude@gmail.com>
2020-10-01 09:46:51 -0700

    First home page test for autocomplete

--- wp1-frontend/cypress/integration/home_page.spec.js
+++ wp1-frontend/cypress/integration/home_page.spec.js
@@ -4,4 +4,16 @@ describe('the home page', () => {
   it('successfully loads', () => {
     cy.visit('/');
   });
+
+  it('autocompletes for Water', () => {
+    cy.visit('/');
+
+    cy.get('.search').type('Water');
+
+    cy.get('.results').should('be.visible');
+    cy.get('.results')
+      .children('li')
+      .eq(1)
+      .should('contain.text', 'Water');
+  });
 });
