38aeadaaa96eb540d7d7170ab930e9e91a92d654
mahakporwal02 <mahakporwal02154@gmail.com>
2021-03-21 20:49:05 +0530

    cypress test for project-table

--- wp1-frontend/cypress/integration/home_page.spec.js
+++ wp1-frontend/cypress/integration/home_page.spec.js
@@ -16,4 +16,20 @@ describe('the home page', () => {
       .eq(1)
       .should('contain.text', 'Water');
   });
+
+  it('project-table for Alien displayed', () => {
+    cy.visit('/');
+
+    cy.get('.search').type('Alien');
+
+    cy.get('.results').should('be.visible');
+    cy.get('.results')
+      .children('li')
+      .eq(0)
+      .should('contain.text', 'Alien')
+      .click();
+
+    cy.get('table').should('be.visible')
+      .should('contain.text','Alien articles by quality and importance');
+  });
 });
