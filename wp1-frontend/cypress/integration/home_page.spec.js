2719697314e6aed6fab57f63e8e49441d3a57265
Travis Briggs <audiodude@gmail.com>
2021-03-27 14:20:32 -0700

    Simplify test

--- wp1-frontend/cypress/integration/home_page.spec.js
+++ wp1-frontend/cypress/integration/home_page.spec.js
@@ -18,22 +18,13 @@ describe('the home page', () => {
   });
 
   it('project-table for Alien displayed', () => {
-    cy.visit('/');
-
-    cy.get('.search').type('Alien');
-
-    cy.get('.results').should('be.visible');
-    cy.get('.results')
-      .children('li')
-      .eq(0)
-      .should('contain.text', 'Alien');
-      
     cy.intercept('/v1/projects/Alien/table').as('table');
     cy.visit('/#/project/Alien');
-    cy.wait('@table', {timeout: 15000});
+    cy.wait('@table', { timeout: 15000 });
 
-    cy.get('table').should('be.visible')
+    cy.get('table')
+      .should('be.visible')
       .eq(0)
-      .should('contain.text','Alien articles by quality and importance')
+      .should('contain.text', 'Alien articles by quality and importance');
   });
 });
