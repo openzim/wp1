9cff860f14eb89624c986bbb6143523199617cbf
Travis Briggs <audiodude@gmail.com>
2021-03-31 18:47:54 -0700

    Add wait on XHR to article_page spec and reformat file

--- wp1-frontend/cypress/integration/article_page.spec.js
+++ wp1-frontend/cypress/integration/article_page.spec.js
@@ -1,16 +1,27 @@
 /// <reference types="Cypress" />
 
 describe('the article page', () => {
-    it('filters by article name', () => {
-        cy.visit('/#/project/Alien/articles');
-        
-        cy.contains('a', 'Filter by article name').click();
+  it('filters by article name', () => {
+    cy.visit('/#/project/Alien/articles');
+    cy.intercept('v1/projects/Alien/articles?articlePattern=Predator').as(
+      'predatorArticles'
+    );
 
-        cy.get('input').eq(2).type('Predator');
-        cy.get('button').eq(3).click();
+    cy.contains('a', 'Filter by article name').click();
 
-        cy.get('table').find('tr').each(($el) => {
-            cy.wrap($el).should('contain.text', 'Predator');
-        });
-    });
+    cy.get('input')
+      .eq(2)
+      .type('Predator');
+    cy.get('button')
+      .eq(3)
+      .click();
+
+    cy.wait('@predatorArticles');
+
+    cy.get('table')
+      .find('tr')
+      .each($el => {
+        cy.wrap($el).should('contain.text', 'Predator');
+      });
+  });
 });
