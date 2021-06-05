4be1b625ffa8cc54ba0536ac94dc014d4e3a80ea
mahakporwal02 <mahakporwal02154@gmail.com>
2021-06-06 04:12:47 +0530

    requested changes done

--- wp1/web/oauth_test.py
+++ wp1/web/oauth_test.py
@@ -3,26 +3,27 @@ from wp1.web.base_web_testcase import BaseWebTestcase
 import unittest
 
 try:
-  # The credentials module isn't checked in and may be missing
   from wp1.credentials import ENV, CREDENTIALS
 except ImportError:
   ENV = None
   CREDENTIALS = None
 
-skip = bool(CREDENTIALS is None or CREDENTIALS[ENV].get('MWOAUTH') is None or
-            CREDENTIALS[ENV]['MWOAUTH'].get('consumer_key') is None or
-            CREDENTIALS[ENV]['MWOAUTH'].get('consumer_secret') is None)
+missing_credentials = CREDENTIALS is None or ENV is None
+if not missing_credentials:
+  mwoauth = CREDENTIALS.get("ENV", {}).get("MWOAUTH", {})
+if missing_credentials or mwoauth.get('consumer_key') is None or mwoauth.get(
+    'consumer_secret') is None:
+  mwoauth = {
+      'consumer_key': 'consumer_key',
+      'consumer_secret': 'consumer_secret'
+  }
 
 
-@unittest.skipIf(skip, 'Cannot find Credentials.py')
 class IdentifyTest(BaseWebTestcase):
 
-  def test_identify(self):
+  def test_identify_authorized_user(self):
     self.app = create_app()
-    self.app.testing = True
     with self.app.test_client() as client:
-      rv = client.get('/v1/oauth/identify')
-      self.assertEqual('401 UNAUTHORIZED', rv.status)
       with client.session_transaction() as sess:
         sess['user'] = {
             'access_token': 'access_token',
@@ -33,12 +34,15 @@ class IdentifyTest(BaseWebTestcase):
       rv = client.get('/v1/oauth/identify')
       self.assertEqual({"username": 'WP1_user'}, rv.get_json())
 
-  def test_logout(self):
+  def test_identify_unauthorized_user(self):
     self.app = create_app()
-    self.app.testing = True
     with self.app.test_client() as client:
-      rv = client.get('/v1/oauth/logout')
-      self.assertEqual('404 NOT FOUND', rv.status)
+      rv = client.get('/v1/oauth/identify')
+      self.assertEqual('401 UNAUTHORIZED', rv.status)
+
+  def test_logout_authorized_user(self):
+    self.app = create_app()
+    with self.app.test_client() as client:
       with client.session_transaction() as sess:
         sess['user'] = {
             'access_token': 'access_token',
@@ -48,3 +52,9 @@ class IdentifyTest(BaseWebTestcase):
         }
       rv = client.get('/v1/oauth/logout')
       self.assertEqual({'status': '204'}, rv.get_json())
+
+  def test_logout_unauthorized_user(self):
+    self.app = create_app()
+    with self.app.test_client() as client:
+      rv = client.get('/v1/oauth/logout')
+      self.assertEqual('404 NOT FOUND', rv.status)
