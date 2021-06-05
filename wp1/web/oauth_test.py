078eba5c74f9623c374278f8d01da3b9a5f214ad
mahakporwal02 <mahakporwal02154@gmail.com>
2021-06-05 18:11:05 +0530

    oauth tests

--- wp1/web/oauth_test.py
+++ wp1/web/oauth_test.py
@@ -1,47 +1,53 @@
 from wp1.web.app import create_app
-from wp1.web.oauth import identify
 from wp1.web.base_web_testcase import BaseWebTestcase
-from unittest import mock
+import unittest
 
-from unittest.mock import patch
+try:
+  # The credentials module isn't checked in and may be missing
+  from wp1.credentials import ENV, CREDENTIALS
+except ImportError:
+  ENV = None
+  CREDENTIALS = None
+
+if (CREDENTIALS is None or CREDENTIALS[ENV].get('MWOAUTH') is None or
+    CREDENTIALS[ENV]['MWOAUTH'].get('consumer_key') is None or
+    CREDENTIALS[ENV]['MWOAUTH'].get('consumer_secret') is None):
+  Skip = True
+else:
+  Skip = False
 
 
+@unittest.skipIf(Skip, 'Cannot find Credentials.py')
 class IdentifyTest(BaseWebTestcase):
 
   def test_identify(self):
     self.app = create_app()
     self.app.testing = True
-    with self.override_db(self.app), self.app.test_client() as c:
-      with c.session_transaction() as sess:
-        sess['user'] = {
-            'access_token': 'abcd',
-            'identity': {
-                'username': 'mahakporwal02'
-            }
-        }
-        assert identify() == {"username": 'mahakporwal02'}
-
-  @mock.patch("wp1.web.oauth.session.get")
-  def identify_test(self, mock_session_get):
-    self.app = create_app()
-    with self.override_db(self.app), self.app.test_client() as client:
-      mock_session_get.return_value = mock.Mock({
-          "access_code": 'abcd',
-          "user": {
-              "username": 'mahakporwal02'
-          }
-      })
-      assert identify() == {"username": 'mahakporwal02'}
-
-  def identify_test(self):
-    self.app = create_app()
-    self.app.testing = True
-    with self.override_db(self.app), self.app.test_client() as client:
+    with self.app.test_client() as client:
+      rv = client.get('/v1/oauth/identify')
+      self.assertEqual('401 UNAUTHORIZED', rv.status)
       with client.session_transaction() as sess:
         sess['user'] = {
-            "access_code": 'abcd',
-            "identity": {
-                "username": 'mahakporwal02'
+            'access_token': 'access_token',
+            'identity': {
+                'username': 'WP1_user'
             }
         }
-        rv = client.get('/v1/oauth/identify')
+      rv = client.get('/v1/oauth/identify')
+      self.assertEqual({"username": 'WP1_user'}, rv.get_json())
+
+  def test_logout(self):
+    self.app = create_app()
+    self.app.testing = True
+    with self.app.test_client() as client:
+      rv = client.get('/v1/oauth/logout')
+      self.assertEqual('404 NOT FOUND', rv.status)
+      with client.session_transaction() as sess:
+        sess['user'] = {
+            'access_token': 'access_token',
+            'identity': {
+                'username': 'WP1_user'
+            }
+        }
+      rv = client.get('/v1/oauth/logout')
+      self.assertEqual({'status': '204'}, rv.get_json())
