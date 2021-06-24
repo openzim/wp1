b0d9f966274ced9a15f691d7b46bcfbf2326ca4e
mahakporwal02 <mahakporwal02154@gmail.com>
2021-06-25 03:55:09 +0530

    changes to cypress tests

--- wp1-frontend/cypress/integration/createList_page.spec.js
+++ wp1-frontend/cypress/integration/createList_page.spec.js
@@ -2,24 +2,30 @@
 
 describe('the createList page', () => {
   it('successfully loads', () => {
+    cy.intercept('v1/sites/', { fixture: 'sites.json' });
     cy.visit('/#/selection/lists/simple/new');
-    cy.get('nav').contains('Create Simple List');
   });
 
   it('displays wiki projects', () => {
-    cy.get('select').select('ab.wikipedia.org');
+    cy.get('select').contains('aa.wikipedia.org');
+    cy.get('select').contains('en.wiktionary.org');
+    cy.get('select').contains('en.wikipedia.org');
   });
 
-  it('displays List Name', () => {
+  it('displays list name', () => {
+    cy.get('#saveListButton').click();
+    cy.on('window:alert', txt => {
+      expect(txt).to.contains('Please fill in this field');
+    });
     cy.get('#listName')
       .click()
       .type('My List');
   });
 
   it('displays textbox', () => {
-    cy.get('textarea')
-      .click()
-      .type('Items');
     cy.get('#saveListButton').click();
+    cy.on('window:alert', txt => {
+      expect(txt).to.contains('Please fill in this field');
+    });
   });
 });
