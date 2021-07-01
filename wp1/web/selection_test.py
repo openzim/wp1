deddb7d40ab59fbf92b88866faed234e222f4388
mahakporwal02 <mahakporwal02154@gmail.com>
2021-07-02 01:25:19 +0530

    frontend changes for events

--- wp1/web/selection_test.py
+++ wp1/web/selection_test.py
@@ -1,8 +1,5 @@
 import unittest
 from wp1.web.app import create_app
-from wp1.web.base_web_testcase import BaseWebTestcase
-from wp1.logic import selection
-import json
 
 
 class ProjectTest(unittest.TestCase):
@@ -20,7 +17,7 @@ class ProjectTest(unittest.TestCase):
   valid_article_name = "Eiffel_Tower\nStatue of Liberty"
   successful_response = {"status": "200", "success": True, "items": {}}
 
-  def test_get_unsuccessful_article_name(self):
+  def test_create_unsuccessful(self):
     self.app = create_app()
     with self.app.test_client() as client:
       rv = client.post('/v1/selection/simple',
@@ -31,7 +28,7 @@ class ProjectTest(unittest.TestCase):
                        })
       self.assertEqual(rv.get_json(), self.unsuccessful_response)
 
-  def test_get_successful_article_name(self):
+  def test_create_successful(self):
     self.app = create_app()
     with self.app.test_client() as client:
       rv = client.post('/v1/selection/simple',
@@ -41,3 +38,36 @@ class ProjectTest(unittest.TestCase):
                            'project_name': 'my_project'
                        })
       self.assertEqual(rv.get_json(), self.successful_response)
+
+  def test_empty_article(self):
+    self.app = create_app()
+    with self.app.test_client() as client:
+      rv = client.post('/v1/selection/simple',
+                       json={
+                           'article_name': '',
+                           'list_name': 'my_list',
+                           'project_name': 'my_project'
+                       })
+    self.assertEqual(rv.status, '400 BAD REQUEST')
+
+  def test_empty_list(self):
+    self.app = create_app()
+    with self.app.test_client() as client:
+      rv = client.post('/v1/selection/simple',
+                       json={
+                           'article_name': self.valid_article_name,
+                           'list_name': '',
+                           'project_name': 'my_project'
+                       })
+    self.assertEqual(rv.status, '400 BAD REQUEST')
+
+  def test_empty_project(self):
+    self.app = create_app()
+    with self.app.test_client() as client:
+      rv = client.post('/v1/selection/simple',
+                       json={
+                           'article_name': self.valid_article_name,
+                           'list_name': 'my_list',
+                           'project_name': ''
+                       })
+    self.assertEqual(rv.status, '400 BAD REQUEST')
