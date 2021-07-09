f4a4fe0ea3ac101381bb8a06ec02e8d537583a68
mahakporwal02 <mahakporwal02154@gmail.com>
2021-07-09 17:52:37 +0530

    ui for saved lists

--- wp1-frontend/src/components/MyLists.vue
+++ wp1-frontend/src/components/MyLists.vue
@@ -3,8 +3,37 @@
     <div>
       <SecondaryNav></SecondaryNav>
     </div>
-    <div class="container">
-      <h1>Coming soon -- User Selections</h1>
+    <div class="row">
+      <div class=" col-lg-6">
+        <div v-for="item in list" :key="item.name" class="card text-center m-5">
+          <div class="card-header">
+            {{ item.name }}
+          </div>
+          <div class="card-body ">
+            <div class="input-group input mx-auto mb-3">
+              <input :id="item.name" :value="item.link" class="form-control" />
+              <div class="input-group-append">
+                <button
+                  class="btn btn-outline-secondary"
+                  v-on:click="copyText(item.name)"
+                >
+                  Copy
+                </button>
+              </div>
+            </div>
+
+            <a :href="item.link" class="btn btn-primary m-2" download
+              >Download .TSV</a
+            >
+            <a :href="item.link" class="btn btn-secondary" download
+              >Download .XLS</a
+            >
+          </div>
+          <div class="card-footer text-muted">
+            {{ item.timstamp }}
+          </div>
+        </div>
+      </div>
     </div>
   </div>
   <div v-else>
@@ -14,12 +43,46 @@
 
 <script>
 import SecondaryNav from './SecondaryNav.vue';
-import LoginRequired from './LoginRequired';
 
 export default {
-  components: { SecondaryNav, LoginRequired },
-  name: 'MyLists'
+  components: { SecondaryNav },
+  name: 'MyLists',
+  data: function() {
+    return {
+      list: [
+        {
+          name: 'My-List1',
+          link: '<URL1>',
+          timstamp: '2 days ago '
+        },
+        {
+          name: 'My-List2',
+          link: '<URL2>',
+          timstamp: '2 days ago'
+        },
+        {
+          name: 'My-List3',
+          link: '<URL3>',
+          timstamp: '2 days ago'
+        },
+        {
+          name: 'My-List4',
+          link: '<URL4>',
+          timstamp: '2 days ago'
+        }
+      ]
+    };
+  },
+  methods: {
+    copyText: function(id) {
+      var copyText = document.getElementById(id);
+      copyText.select();
+      document.execCommand('copy');
+    }
+  }
 };
 </script>
 
-<style scoped></style>
+<style scoped>
+@import '../cards.scss';
+</style>
