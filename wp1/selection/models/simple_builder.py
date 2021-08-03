4b391effd5fd10ddbf83f9a0356bf1dafacf7920
mahakporwal02 <mahakporwal02154@gmail.com>
2021-08-04 02:36:05 +0530

    fixed typos and deleted selection_test.py

--- wp1/selection/models/simple_builder.py
+++ wp1/selection/models/simple_builder.py
@@ -1,4 +1,5 @@
 import urllib.parse
+from wp1.constants import CONTENT_TYPE_TO_EXT
 from wp1.selection.abstract_builder import AbstractBuilder
 
 
@@ -9,41 +10,44 @@ class SimpleBuilder(AbstractBuilder):
       return ([], [], ['Empty List'])
     invalid_article_names = []
     valid_article_names = []
-    forbiden_chars = []
+    forbidden_chars = []
+    error_msg = []
     for item in params['list']:
       is_valid = True
       item = item.strip().replace(" ", "_")
       decoded_item = urllib.parse.unquote(item)
       len_item = len(decoded_item.encode("utf-8"))
       char_set = ["#", "<", ">", "[", "]", "{", "}", "|"]
-      if len_item > 256:
-        forbiden_chars.append('length greater than 256 bytes')
-        invalid_article_names.append(decoded_item)
-        is_valid = False
-        continue
       for forbiden_character in char_set:
         if forbiden_character in decoded_item:
-          forbiden_chars.append(forbiden_character)
+          forbidden_chars.append(forbiden_character)
           if is_valid:
             invalid_article_names.append(decoded_item)
             is_valid = False
+      if len_item > 256:
+        error_msg.append('The list contained an item longer than 256 bytes')
+        invalid_article_names.append(decoded_item)
+        is_valid = False
       if is_valid:
         article_name = decoded_item.replace(
             "https://en.wikipedia.org/wiki/",
             "").replace("https://en.wikipedia.org/w/index.php?title=", "")
         valid_article_names.append(article_name)
-    return (valid_article_names, invalid_article_names, forbiden_chars)
+    if forbidden_chars:
+      error_msg.append('The list contained the following invalid characters: ' +
+                       ', '.join(forbidden_chars))
+    return (valid_article_names, invalid_article_names, error_msg)
 
   def build(self, content_type, **params):
+    if content_type != 'text/tab-separated-values':
+      raise ValueError('Unrecognized content type')
     if len(params) == 1:
-      if 'list' in params.keys():
+      if 'list' in params:
         return '\n'.join(params['list']).encode('utf-8')
-      KeyError('Incorrect params present')
-    ValueError('Unwanted params present')
+      raise ValueError(
+          f'Missing required param: list, unnecessary argument given: {list(params.keys())[0]}'
+      )
+    raise ValueError('Additional unnecessary params present')
 
   def validate(self, **params):
-    valid_names, invalid_names, forbiden_chars = self._validate_list(**params)
-    if forbiden_chars[0] != 'Empty List':
-      forbiden_chars.insert(
-          0, 'The list contained the following invalid characters: ')
-    return (valid_names, invalid_names, forbiden_chars)
+    return self._validate_list(**params)
