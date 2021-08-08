5ee3e86feb14888011dd36cd0673f741e2373fac
mahakporwal02 <mahakporwal02154@gmail.com>
2021-08-09 03:21:22 +0530

    added methods for saving simple lists

--- db/migrations/20210807_01_XJESg-rename-builder-columns.py
+++ db/migrations/20210807_01_XJESg-rename-builder-columns.py
@@ -7,12 +7,21 @@ from yoyo import step
 __depends__ = {'20210728_01_eJYGE-add-model-to-builders'}
 
 steps = [
-    step("ALTER TABLE builders"
-         "RENAME COLUMN s_id TO b_id, "
-         "RENAME COLUMN s_name TO b_name, "
-         "RENAME COLUMN s_user_id TO b_user_id, "
-         "RENAME COLUMN s_project TO b_project, "
-         "RENAME COLUMN s_params TO b_params, "
-         "RENAME COLUMN s_created_at TO b_created_at, "
-         "RENAME COLUMN s_updated_at TO b_updated_at")
+    step(
+        "ALTER TABLE builders "
+        "CHANGE COLUMN s_id b_id INTEGER AUTO_INCREMENT, "
+        "CHANGE COLUMN s_name b_name VARBINARY(255), "
+        "CHANGE COLUMN s_user_id b_user_id INTEGER, "
+        "CHANGE COLUMN s_project b_project VARBINARY(255), "
+        "CHANGE COLUMN s_params b_params BLOB, "
+        "CHANGE COLUMN s_created_at b_created_at BINARY(20), "
+        "CHANGE COLUMN s_updated_at b_updated_at BINARY(20)",
+        "ALTER TABLE builders "
+        "CHANGE COLUMN b_id s_id INTEGER AUTO_INCREMENT, "
+        "CHANGE COLUMN b_name s_name VARBINARY(255), "
+        "CHANGE COLUMN b_user_id s_user_id INTEGER, "
+        "CHANGE COLUMN b_project s_project VARBINARY(255), "
+        "CHANGE COLUMN b_params s_params BLOB, "
+        "CHANGE COLUMN b_created_at s_created_at BINARY(20), "
+        "CHANGE COLUMN b_updated_at s_updated_at BINARY(20)")
 ]
--- wp1/web/selection.py
+++ wp1/web/selection.py
@@ -1,6 +1,7 @@
-from wp1.selection.models.simple_builder import SimpleBuilder
 import flask
+from wp1.selection.models.simple_builder import SimpleBuilder
 from wp1.web import authenticate
+from wp1.wp10_db import connect as wp10_connect
 
 selection = flask.Blueprint('selection', __name__)
 
@@ -17,6 +18,8 @@ def create():
     flask.abort(400)
   simple_builder = SimpleBuilder()
   valid_names, invalid_names, errors = simple_builder.validate(**params)
+  wp10db = wp10_connect()
+  simple_builder.insert_builder(data, wp10db)
   if invalid_names:
     return {
         "success": False,
@@ -27,3 +30,19 @@ def create():
         }
     }
   return {"success": True, "items": {}}
+
+
+@selection.route('/simple/lists')
+@authenticate
+def get_list_data():
+  list_data = []
+  simple_builder = SimpleBuilder()
+  wp10db = wp10_connect()
+  article_lists = simple_builder.get_lists(wp10db)
+  for article_data in article_lists['article_lists']:
+    list_data.append({
+        'list_name': article_data['b_name'].decode('utf-8'),
+        'project_name': article_data['b_project'].decode('utf-8'),
+        'created_at': article_data['b_created_at'].decode('utf-8')
+    })
+  return {'list_data': list_data}
