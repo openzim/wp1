17cd0e494660de06d23a21405463fde002dc42aa
Travis Briggs <audiodude@gmail.com>
2021-08-22 17:21:26 -0700

    Further refactoring of selection getting object key

--- wp1/logic/selection.py
+++ wp1/logic/selection.py
@@ -13,14 +13,21 @@ def insert_selection(wp10db, selection):
   wp10db.commit()
 
 
+def object_key_for(selection_id, content_type, model):
+  if not selection_id:
+    raise ValueError('Cannot get object key for None selection_id')
+  if not model:
+    raise ValueError('Expected WP1 model name, got: %r' % model)
+  ext = CONTENT_TYPE_TO_EXT.get(content_type, '???')
+  return 'selections/%(model)s/%(id)s.%(ext)s' % {
+      'model': model,
+      'id': selection_id,
+      'ext': ext,
+  }
+
+
 def object_key_for_selection(selection, model):
   if not selection:
     raise ValueError('Cannot get object key for None selection')
-  if not model:
-    raise ValueError('Expected WP1 model name, got: %r' % model)
-  ext = CONTENT_TYPE_TO_EXT.get(selection.s_content_type, '???')
-  return 'selections/%(model)s/%(id)s.%(ext)s' % {
-      'model': model,
-      'id': selection.s_id.decode('utf-8'),
-      'ext': ext,
-  }
+  return object_key_for(selection.s_id.decode('utf-8'),
+                        selection.s_content_type.decode('utf-8'), model)
