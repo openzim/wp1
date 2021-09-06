dae8911479e2931293b1b1c39ba005bea6977030
Travis Briggs <audiodude@gmail.com>
2021-09-06 14:11:31 -0700

    Add python tests for new and refactored methods

--- wp1/web/selection_test.py
+++ wp1/web/selection_test.py
@@ -4,7 +4,6 @@ from wp1.web.base_web_testcase import BaseWebTestcase
 
 
 class SelectionTest(BaseWebTestcase):
-
   USER = {
       'access_token': 'access_token',
       'identity': {
@@ -12,24 +11,13 @@ class SelectionTest(BaseWebTestcase):
           'sub': '1234',
       },
   }
-  invalid_article_name = "Eiffel_Tower\nStatue of#Liberty"
-  unsuccessful_response = {
-      "success": False,
-      "items": {
-          'valid': ['Eiffel_Tower'],
-          'invalid': ['Statue_of#Liberty'],
-          'errors': ['The list contained the following invalid characters: #'],
-      },
-  }
-  valid_article_name = "Eiffel_Tower\nStatue of Liberty"
-  successful_response = {"success": True, "items": {}}
 
   expected_list_data = {
       'builders': [{
           'id':
               1,
           'name':
-              'list_name',
+              'name',
           'project':
               'project_name',
           'selections': [{
@@ -46,7 +34,7 @@ class SelectionTest(BaseWebTestcase):
           'id':
               1,
           'name':
-              'list_name',
+              'name',
           'project':
               'project_name',
           'selections': [{
@@ -66,87 +54,11 @@ class SelectionTest(BaseWebTestcase):
   expected_lists_with_no_selections = {
       'builders': [{
           'id': 1,
-          'name': 'list_name',
+          'name': 'name',
           'project': 'project_name',
           'selections': []
       }]
   }
-
-  def test_create_unsuccessful(self):
-    self.app = create_app()
-    with self.app.test_client() as client:
-      with client.session_transaction() as sess:
-        sess['user'] = self.USER
-      rv = client.post('/v1/selection/simple',
-                       json={
-                           'articles': self.invalid_article_name,
-                           'list_name': 'my_list',
-                           'project': 'my_project'
-                       })
-      self.assertEqual(rv.get_json(), self.unsuccessful_response)
-
-  def test_create_successful(self):
-    self.app = create_app()
-    with self.app.test_client() as client, self.override_db(self.app):
-      with client.session_transaction() as sess:
-        sess['user'] = self.USER
-      rv = client.post('/v1/selection/simple',
-                       json={
-                           'articles': self.valid_article_name,
-                           'list_name': 'my_list',
-                           'project': 'my_project'
-                       })
-      self.assertEqual(rv.get_json(), self.successful_response)
-
-  def test_empty_article(self):
-    self.app = create_app()
-    with self.app.test_client() as client:
-      with client.session_transaction() as sess:
-        sess['user'] = self.USER
-      rv = client.post('/v1/selection/simple',
-                       json={
-                           'articles': '',
-                           'list_name': 'my_list',
-                           'project': 'my_project'
-                       })
-      self.assertEqual(rv.status, '400 BAD REQUEST')
-
-  def test_empty_list(self):
-    self.app = create_app()
-    with self.app.test_client() as client:
-      with client.session_transaction() as sess:
-        sess['user'] = self.USER
-      rv = client.post('/v1/selection/simple',
-                       json={
-                           'articles': self.valid_article_name,
-                           'list_name': '',
-                           'project': 'my_project'
-                       })
-      self.assertEqual(rv.status, '400 BAD REQUEST')
-
-  def test_empty_project(self):
-    self.app = create_app()
-    with self.app.test_client() as client:
-      with client.session_transaction() as sess:
-        sess['user'] = self.USER
-      rv = client.post('/v1/selection/simple',
-                       json={
-                           'articles': self.valid_article_name,
-                           'list_name': 'my_list',
-                           'project': ''
-                       })
-      self.assertEqual(rv.status, '400 BAD REQUEST')
-
-  def test_selection_unauthorized_user(self):
-    self.app = create_app()
-    with self.app.test_client() as client:
-      rv = client.post('/v1/selection/simple',
-                       json={
-                           'articles': self.valid_article_name,
-                           'list_name': 'my_list',
-                           'project': ''
-                       })
-    self.assertEqual('401 UNAUTHORIZED', rv.status)
 
   def test_get_list_data(self):
     self.app = create_app()
@@ -154,7 +66,7 @@ class SelectionTest(BaseWebTestcase):
       with self.wp10db.cursor() as cursor:
         cursor.execute('''INSERT INTO builders
         (b_name, b_user_id, b_project, b_model)
-        VALUES ('list_name', '1234', 'project_name', 'model')
+        VALUES ('name', '1234', 'project_name', 'model')
       ''')
         cursor.execute(
             'INSERT INTO selections VALUES (1, 1, "text/tab-separated-values", "20201225105544")'
@@ -171,7 +83,7 @@ class SelectionTest(BaseWebTestcase):
       with self.wp10db.cursor() as cursor:
         cursor.execute('''INSERT INTO builders
         (b_name, b_user_id, b_project, b_model)
-        VALUES ('list_name', '1234', 'project_name', 'model')
+        VALUES ('name', '1234', 'project_name', 'model')
       ''')
         cursor.execute(
             'INSERT INTO selections VALUES (1, 1, "text/tab-separated-values", "20201225105544")'
@@ -192,7 +104,7 @@ class SelectionTest(BaseWebTestcase):
       with self.wp10db.cursor() as cursor:
         cursor.execute('''INSERT INTO builders
         (b_name, b_user_id, b_project, b_model)
-        VALUES ('list_name', '1234', 'project_name', 'model')
+        VALUES ('name', '1234', 'project_name', 'model')
       ''')
       self.wp10db.commit()
       with client.session_transaction() as sess:
