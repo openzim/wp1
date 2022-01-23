532db161de3c898f683ec85528986758bc5936ed
Travis Briggs <audiodude@gmail.com>
2022-01-23 13:37:25 -0800

    Fix Cypress tests

--- wp1-frontend/cypress/integration/myLists_page.spec.js
+++ wp1-frontend/cypress/integration/myLists_page.spec.js
@@ -4,25 +4,28 @@ describe('when the user is logged in', () => {
   beforeEach(() => {
     cy.intercept('v1/selection/simple/lists', { fixture: 'list_data.json' });
     cy.intercept('v1/oauth/identify', { fixture: 'identity.json' });
-  });
-
-  it('successfully loads', () => {
     cy.visit('/#/selections/user');
   });
 
-  it('displays list and its contents', () => {
-    cy.get('.row').contains('list_name');
-    cy.get('.row').contains('en.wikipedia.org');
-    cy.get('.btn-primary').contains('Download tsv');
+  it('successfully loads', () => {});
+
+  it('displays the datatables view', () => {
+    cy.get('.dataTables_info').contains('Showing 1 to 1 of 1 entries');
   });
 
-  it('copy button works', () => {
-    cy.get('.input-group-append > .btn')
-      .contains('Copy')
-      .click();
-    cy.get('#1')
-      .invoke('val')
-      .should(val => expect(val).to.eql('https://www.example.com/<id>'));
+  it('displays list and its contents', () => {
+    cy.get('#list-table td').contains('list_name');
+    cy.get('#list-table td').contains('en.wikipedia.org');
+    cy.get('#list-table td').contains('9/5/2021 2:28:23 PM');
+    cy.get('#list-table td').contains('9/5/2021 2:45:03 PM');
+    cy.get('#list-table td').contains('9/5/2021 3:18:23 PM');
+    cy.get('#list-table td .btn-primary').contains('Edit');
+  });
+
+  it('takes the user to the edit screen when edit is clicked', () => {
+    cy.intercept('GET', 'v1/builders/1', { fixture: 'builder_1.json' });
+    cy.get('#list-table td .btn-primary').click();
+    cy.url().should('eq', 'http://localhost:3000/#/selections/simple/1');
   });
 });
 
