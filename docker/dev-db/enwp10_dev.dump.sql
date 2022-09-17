a4047d5022790f48b7c0b870aebb681fe26f9409
Travis Briggs <audiodude@gamil.com>
2022-09-16 21:00:35 -0700

    Update dev db with latest migrations

--- docker/dev-db/enwp10_dev.dump.sql
+++ docker/dev-db/enwp10_dev.dump.sql
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:17b6572baf4b8ba18af6a8ae636175afbf6b96a9797624f58c9404a8a7e8ce20
-size 31085390
+oid sha256:29af196907f73161bac17a54bf2516d7e65fc67a9b43c23c2f8ceefe346bddc9
+size 31085787
