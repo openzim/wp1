3be20203d7c50c1199577b7bf83224f55ae26afa
Travis Briggs <audiodude@gmail.com>
2023-04-06 08:30:19 -0700

    Upgrade to Flask 2.2.3, switch to Flask-Session2, and drop rq-dashboard (#601)

--- wp1/web/app_test.py
+++ wp1/web/app_test.py
@@ -19,29 +19,3 @@ class AppTest(BaseWebTestcase):
     with self.override_db(self.app), self.app.test_client() as client:
       rv = client.get('/v1/openapi.yml')
       self.assertTrue(b"title: 'WP 1.0 Frontend'" in rv.data)
-
-
-class RqTest(unittest.TestCase):
-
-  def test_rq_no_login(self):
-    os.environ['RQ_USER'] = 'testuser'
-    os.environ['RQ_PASS'] = 'testpass'  # nosec
-    self.app = create_app()
-    with self.app.test_client() as client:
-      rv = client.get('/rq', follow_redirects=True)
-      self.assertTrue(b'Please login' in rv.data)
-
-  def test_rq_login(self):
-    os.environ['RQ_USER'] = 'testuser'
-    os.environ['RQ_PASS'] = 'testpass'  # nosec
-    self.app = create_app()
-    with self.app.test_client() as client:
-      rv = client.get(
-          '/rq',
-          headers={
-              'Authorization':
-                  'Basic {}'.format(
-                      b64encode(b'testuser:testpass').decode('utf-8'))
-          },
-          follow_redirects=True)
-      self.assertFalse(b'Please login' in rv.data)
