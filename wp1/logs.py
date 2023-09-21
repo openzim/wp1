e9606d472901ff94cf6faac3ea9b1607261b455b
Travis Briggs <audiodude@gmail.com>
2023-09-20 19:38:02 -0700

    Use <noinclude> for {{Log}} message in logs

--- wp1/logs.py
+++ wp1/logs.py
@@ -207,7 +207,7 @@ def update_log_page_for_project(project_name):
 
     p = api.get_page(log_page_name(project_name))
 
-    header = ('{{Log}}\n')
+    header = ('<noinclude>{{Log}}</noinclude>\n')
 
     if len(edits) == 0:
       today = get_current_datetime()
