93bf6153d0339727afe3e5298b73de50333503c4
Travis Briggs <audiodude@gmail.com>
2024-03-17 06:41:00 -0700

    Remove outdated vite-plugin-vue2 and configure via @viejs/plugin-vue2

--- wp1-frontend/vite.config.js
+++ wp1-frontend/vite.config.js
@@ -1,5 +1,5 @@
 import { defineConfig } from 'vite';
-import { createVuePlugin as vue } from 'vite-plugin-vue2';
+import vue from '@vitejs/plugin-vue2';
 import inject from '@rollup/plugin-inject';
 
 const path = require('path');
