f3d51b5b15ce8e3a13939dc8bf7696d59e33cfff
Travis Briggs <audiodude@gmail.com>
2024-04-30 14:41:59 +0200

    Functions for streaming page view data

--- wp1/scores.py
+++ wp1/scores.py
@@ -1,3 +1,5 @@
+from bz2 import BZ2Decompressor
+
 import csv
 import requests
 
@@ -18,3 +20,37 @@ def wiki_languages():
   next(reader, None)
   for row in reader:
     yield row[2]
+
+
+def raw_pageviews(decode=True):
+
+  def as_bytes():
+    with requests.get(
+        'https://dumps.wikimedia.org/other/pageview_complete/monthly/2024/2024-03/pageviews-202403-automated.bz2',
+        stream=True) as r:
+
+      decompressor = BZ2Decompressor()
+      trailing = b''
+      # Read data in 1 MB chunks
+      for http_chunk in r.iter_content(chunk_size=1024 * 1024):
+        data = decompressor.decompress(http_chunk)
+        lines = [line for line in data.split(b'\n') if line]
+        if not lines:
+          continue
+
+        yield trailing + lines[0]
+        yield from lines[1:-1]
+        trailing = lines[-1]
+
+  if decode:
+    for line in as_bytes():
+      yield line.decode('utf-8')
+  else:
+    yield from as_bytes()
+
+
+def pageviews_for_lang(lang):
+  needle = f'{lang}.wikipedia'
+  for line in raw_pageviews():
+    if needle in line:
+      yield line
