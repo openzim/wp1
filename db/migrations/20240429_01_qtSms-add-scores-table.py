2baea766409549babd799a814991c767a5a74ae2
Travis Briggs <audiodude@gmail.com>
2024-05-01 14:19:54 +0200

    Re-work page_scores table

--- db/migrations/20240429_01_qtSms-add-scores-table.py
+++ db/migrations/20240429_01_qtSms-add-scores-table.py
@@ -7,16 +7,16 @@ from yoyo import step
 __depends__ = {'20230528_02_pL6ka-add-b-selection-zim-version-to-builders'}
 
 steps = [
-    step('''CREATE TABLE page_scores (
-                ps_project VARBINARY(255),
+    step(
+        '''CREATE TABLE page_scores (
+                ps_lang VARBINARY(255),
                 ps_page_id INTEGER NOT NULL,
-                ps_article VARBINARY(255),
+                ps_article VARBINARY(1024),
                 ps_views INTEGER DEFAULT 0,
                 ps_links INTEGER DEFAULT 0,
                 ps_lang_links INTEGER DEFAULT 0,
                 ps_score INTEGER DEFAULT 0,
-                KEY `page_title` (`ps_project`,  `ps_article`),
-                KEY `page_id` (`ps_page_id`)
-            )''',
-        "DROP TABLE page_scores")
+                PRIMARY KEY (`ps_lang`, `ps_page_id`),
+                KEY `lang_article` (`ps_lang`, `ps_article`)
+            )''', 'DROP TABLE page_scores')
 ]
