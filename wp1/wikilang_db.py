f667497683b12d35c7d8a9319147155cd328be20
Travis Briggs <audiodude@gmail.com>
2024-08-18 12:14:06 -0700

    Add test and fix bugs test found

--- wp1/wikilang_db.py
+++ wp1/wikilang_db.py
@@ -1,9 +1,10 @@
 from functools import partial
 
-from wp1.db import connect
+from wp1.db import connect as db_connect
 
 
 def connect(lang):
-  db = f'{lang}wiki_p'
-  host = f'{db}.analytics.db.svc.eqiad.wmflabs'
-  return connect('WIKIDB', host=replica_host, db=db)
+  wiki = f'{lang}wiki'
+  db = f'{wiki}_p'
+  host = f'{wiki}.analytics.db.svc.eqiad.wmflabs'
+  return db_connect('WIKIDB', host=host, db=db)
