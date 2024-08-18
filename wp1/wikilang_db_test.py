35be6a563a8219bab9c6250f7f4892b93fa1b723
Travis Briggs <audiodude@gmail.com>
2024-08-18 12:14:06 -0700

    Add test and fix bugs test found

---
+++ wp1/wikilang_db_test.py
@@ -0,0 +1,14 @@
+import unittest
+from unittest.mock import patch
+
+import wp1.wikilang_db
+
+
+class WikiLangDbTest(unittest.TestCase):
+
+  @patch('wp1.wikilang_db.db_connect')
+  def test_connect(self, mock_connect):
+    wp1.wikilang_db.connect('fr')
+
+    mock_connect.assert_called_once_with(
+        'WIKIDB', host='frwiki.analytics.db.svc.eqiad.wmflabs', db='frwiki_p')
