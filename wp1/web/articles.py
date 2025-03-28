0aa7178968afb6b89a4388c5691ce48f41f826ae
Mightymanh <dphan8@uic.edu>
2025-03-28 03:51:27 -0500

    Fix broken link bug for article's name that includes slash
    In frontend, double encode the article name.
    In backend, decode the encoded article name (Flask decode the first time, and we decode it the second time to get original name).

--- wp1/web/articles.py
+++ wp1/web/articles.py
@@ -1,4 +1,5 @@
 import flask
+import urllib.parse
 
 from wp1.api import get_page, get_revision_id_by_timestamp
 from wp1.constants import FRONTEND_WIKI_BASE
@@ -6,8 +7,9 @@ from wp1.constants import FRONTEND_WIKI_BASE
 articles = flask.Blueprint('articles', __name__)
 
 
-@articles.route('<name>/<timestamp>/redirect')
-def redirect(name, timestamp):
+@articles.route('<name_encoded>/<timestamp>/redirect')
+def redirect(name_encoded, timestamp):
+  name = urllib.parse.unquote(name_encoded)
   page = get_page(name)
   revid = get_revision_id_by_timestamp(page, timestamp)
 
