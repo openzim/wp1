22ecdb0d96525fd59543ebeff5f5612a1a348f3d
Travis Briggs <audiodude@gmail.com>
2025-04-26 20:33:48 -0700

    Migrate dev db

--- docker/dev-db/enwp10_dev.dump.sql
+++ docker/dev-db/enwp10_dev.dump.sql
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4716cbb9650d8e547df7f990dd1bb775f3985eda08a6f49f3c1cd58ec467b2ba
-size 31105032
+oid sha256:af02d51a6a38b202280f0890fced63545df48373fb4916a1edbf855fd1b8ee80
+size 7528485
