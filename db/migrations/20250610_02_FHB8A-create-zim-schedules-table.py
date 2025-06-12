077dc09f85cb87a59397eec8a54497b08bf6f1f3
Nicola Pace <nicola.pace@cern.ch>
2025-06-12 10:22:33 +0200

    feat: add zim_schedules table, migration script and update dump

---
+++ db/migrations/20250610_02_FHB8A-create-zim-schedules-table.py
@@ -0,0 +1,20 @@
+"""
+This migration creates the zim_schedules table.
+"""
+
+from yoyo import step
+
+__depends__ = {'20250610_01_kNsXy-add-email-to-users-table'}
+
+steps = [
+    step(
+        'CREATE TABLE zim_schedules ('
+        '  s_id VARCHAR(36) NOT NULL PRIMARY KEY,' 
+        '  s_builder_id VARCHAR(255) NOT NULL,' # ID in the builders table
+        '  s_zim_file_id INTEGER NULL,' # Last ZIM file related to the schedule, can be NULL
+        '  s_rq_job_id INTEGER NOT NULL,' # ID of the job in the rq-scheduler
+        '  s_last_updated_at BINARY(14) NOT NULL' # Last update to the schedule
+        ')', 
+        'DROP TABLE zim_schedules'
+    )
+]
