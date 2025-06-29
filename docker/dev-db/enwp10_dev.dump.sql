375348be400adef2690af82d882dcc6a05479826
Nicola Pace <nicola.pace@cern.ch>
2025-06-29 12:36:54 +0200

    fix: change u_email column type to VARCHAR in users table migration and update dump

--- docker/dev-db/enwp10_dev.dump.sql
+++ docker/dev-db/enwp10_dev.dump.sql
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:05f55aaf2f150da5cac6c09156b5528eb3d31fda53ce832d7748d8cbf60f887d
-size 10412816
+oid sha256:a357f7918236272d71cdb4a894fc61ca59f16edf5e341d748761db11a43841e1
+size 10413676
