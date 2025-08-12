46c4e344f63cb9b77b081319b1b4031f5b570943
Nicola Pace <nicola.pace@cern.ch>
2025-08-12 18:07:36 +0200

    fix: change u_email column type to VARCHAR in users table migration and update dump

--- db/migrations/20250610_01_kNsXy-add-email-to-users-table.py
+++ db/migrations/20250610_01_kNsXy-add-email-to-users-table.py
@@ -8,7 +8,7 @@ __depends__ = {'20250426_01_i656U-drop-logging-table'}
 
 steps = [
     step(
-        "ALTER TABLE users ADD COLUMN u_email VARBINARY(255) NULL",
+        "ALTER TABLE users ADD COLUMN u_email VARCHAR(255) NULL",
         "ALTER TABLE users DROP COLUMN u_email"
     )
 ]
