596610c4428bc9bf7039f82adae3652249aa3e6f
Nicola Pace <nicola.pace@cern.ch>
2025-08-12 18:07:36 +0200

    fix: update data types to VARBINARY in users table and zim_schedules table and update dump

--- db/migrations/20250610_02_FHB8A-create-zim-schedules-table.py
+++ db/migrations/20250610_02_FHB8A-create-zim-schedules-table.py
@@ -9,10 +9,10 @@ __depends__ = {'20250610_01_kNsXy-add-email-to-users-table'}
 steps = [
     step(
         'CREATE TABLE zim_schedules ('
-        '  s_id VARCHAR(36) NOT NULL PRIMARY KEY,' 
-        '  s_builder_id VARCHAR(255) NOT NULL,' # ID in the builders table
+        '  s_id VARBINARY(36) NOT NULL PRIMARY KEY,' 
+        '  s_builder_id VARBINARY(255) NOT NULL,' # ID in the builders table
         '  s_zim_file_id INTEGER NULL,' # Last ZIM file related to the schedule, can be NULL
-        '  s_rq_job_id VARCHAR(36) NOT NULL,' # ID of the job in the rq-scheduler
+        '  s_rq_job_id VARBINARY(36) NOT NULL,' # ID of the job in the rq-scheduler
         '  s_interval INTEGER NULL,' # Interval between ZIM generations
         '  s_remaining_generations INTEGER NULL,' # Remaining generations to be created
         '  s_last_updated_at BINARY(14) NOT NULL' # Last update to the schedule
