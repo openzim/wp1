bc15380bfce59a45f6540bd7157fd2673fc71723
Nicola Pace <nicola.pace@cern.ch>
2025-08-12 18:07:35 +0200

    fix: update zim_schedules table schema to correct data types and add missing fields

--- db/migrations/20250610_02_FHB8A-create-zim-schedules-table.py
+++ db/migrations/20250610_02_FHB8A-create-zim-schedules-table.py
@@ -12,7 +12,9 @@ steps = [
         '  s_id VARCHAR(36) NOT NULL PRIMARY KEY,' 
         '  s_builder_id VARCHAR(255) NOT NULL,' # ID in the builders table
         '  s_zim_file_id INTEGER NULL,' # Last ZIM file related to the schedule, can be NULL
-        '  s_rq_job_id INTEGER NOT NULL,' # ID of the job in the rq-scheduler
+        '  s_rq_job_id VARCHAR(36) NOT NULL,' # ID of the job in the rq-scheduler
+        '  s_interval INTEGER NULL,' # Interval between ZIM generations
+        '  s_remaining_generations INTEGER NULL,' # Remaining generations to be created
         '  s_last_updated_at BINARY(14) NOT NULL' # Last update to the schedule
         ')', 
         'DROP TABLE zim_schedules'
