62850c5d0eed887b2853c355ef223ced60f428c9
Uchechukwu Orji <orjiuchechukwu52@yahoo.com>
2025-11-12 14:12:27 +0100

    update zimfarm-api startup command

--- docker/minio/setup-buckets.sh
+++ docker/minio/setup-buckets.sh
@@ -4,7 +4,7 @@ MINIO_ALIAS="dev_minio"
 MINIO_URL="http://minio:9000"
 MINIO_USER="minio_key"
 MINIO_PASSWORD="minio_secret"
-BUCKETS=("org-kiwix-dev-wp1" "org-kiwix-dev-artifacts" "org-kiwix-dev-logs" "org-kiwix-dev-zims", "org-kiwix-dev-cache")
+BUCKETS=("org-kiwix-dev-wp1" "org-kiwix-dev-artifacts" "org-kiwix-dev-logs" "org-kiwix-dev-zims" "org-kiwix-dev-cache")
 
 # Wait for MinIO
 echo "Waiting for MinIO to be ready..."
@@ -12,7 +12,7 @@ for i in $(seq 1 30); do
     # Set up MinIO alias if it's ready
     /usr/bin/mc alias set $MINIO_ALIAS $MINIO_URL $MINIO_USER $MINIO_PASSWORD && echo "MinIO is ready!" && break
     sleep 2
-    [ $i -eq 30 ] && { echo "ERROR: MinIO timeout"; exit 1; }
+    [ "$i" -eq 30 ] && { echo "ERROR: MinIO timeout"; exit 1; }
 done
 
 # Setup buckets
