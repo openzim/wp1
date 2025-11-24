b1934363c8018e1afd0280850c5e7399e5e7f6fb
Travis Briggs <audiodude@gmail.com>
2025-11-23 20:58:00 -0800

    Undo formatting changes

--- wp1/web/selection.py
+++ wp1/web/selection.py
@@ -6,13 +6,13 @@ from wp1.web import authenticate
 from wp1.web.db import get_db
 from wp1.web.storage import get_storage
 
-selection = flask.Blueprint("selection", __name__)
+selection = flask.Blueprint('selection', __name__)
 
 
-@selection.route("/simple/lists")
+@selection.route('/simple/lists')
 @authenticate
 def get_list_data():
-    wp10db = get_db("wp10db")
-    user_id = flask.session["user"]["identity"]["sub"]
-    builders = logic_builder.get_builders_with_selections(wp10db, user_id)
-    return flask.jsonify({"builders": builders})
+  wp10db = get_db('wp10db')
+  user_id = flask.session['user']['identity']['sub']
+  builders = logic_builder.get_builders_with_selections(wp10db, user_id)
+  return flask.jsonify({'builders': builders})
