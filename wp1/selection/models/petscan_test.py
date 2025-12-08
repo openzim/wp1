33046149395709f42ea1c37f571fa490d62b7231
Travis Briggs <audiodude@gmail.com>
2025-12-07 17:56:22 -0800

    Change Petscan selection to use TSV output directly

--- wp1/selection/models/petscan_test.py
+++ wp1/selection/models/petscan_test.py
@@ -9,17 +9,8 @@ from wp1.selection.models.petscan import Builder as PetscanBuilder
 
 
 class PetscanBuilderTest(BaseWpOneDbTest):
-  mock_petscan_response = {
-      '*': [{
-          'a': {
-              '*': [{
-                  'title': 'Foo'
-              }, {
-                  'title': 'Bar'
-              }]
-          }
-      }]
-  }
+
+  mock_response = 'Foo\nBar'
 
   def setUp(self):
     super().setUp()
@@ -32,20 +23,20 @@ class PetscanBuilderTest(BaseWpOneDbTest):
         b_model=b'wp1.selection.models.petscan',
         b_params='{"url":"https://petscan.wmcloud.org/?psid=552"}')
     self.builder = PetscanBuilder()
-
-  @patch('wp1.selection.models.petscan.requests')
-  def test_materialize(self, mock_requests):
+  
+  @patch('wp1.selection.models.petscan.requests.get')
+  def test_materialize(self, mock_requests_get):
+    mock_requests_get.return_value.text = self.mock_response
+    
     self.builder.materialize(self.s3, self.wp10db, self.builder_model,
                              'text/tab-separated-values', 1)
     actual = get_first_selection(self.wp10db)
     self.assertEqual(actual.s_content_type, b'text/tab-separated-values')
     self.assertEqual(actual.s_builder_id, b'1a-2b-3c-4d')
 
-  @patch('wp1.selection.models.petscan.requests')
-  def test_build(self, mock_requests):
-    mock_response = MagicMock()
-    mock_response.json.return_value = self.mock_petscan_response
-    mock_requests.get.return_value = mock_response
+  @patch('wp1.selection.models.petscan.requests.get')
+  def test_build(self, mock_requests_get):
+    mock_requests_get.return_value.text = self.mock_response
 
     actual = self.builder.build('text/tab-separated-values',
                                 url='https://petscan.wmcloud.org.fake/?psid=42')
@@ -53,56 +44,51 @@ class PetscanBuilderTest(BaseWpOneDbTest):
 
   def test_build_wrong_content_type(self):
     with self.assertRaises(Wp1FatalSelectionError):
-      actual = self.builder.build(
+      self.builder.build(
           None, url='https://petscan.wmcloud.org.fake/?psid=42')
 
   def test_build_missing_url(self):
     with self.assertRaises(Wp1FatalSelectionError):
-      actual = self.builder.build('text/tab-separated-values')
+      self.builder.build('text/tab-separated-values')
 
   def test_build_url_not_str(self):
     with self.assertRaises(Wp1FatalSelectionError):
-      actual = self.builder.build(
+      self.builder.build(
           'text/tab-separated-values',
           url=['https://petscan.wmcloud.org.fake/?psid=42'])
 
-  @patch('wp1.selection.models.petscan.requests')
-  def test_build_no_format(self, mock_requests):
-    mock_response = MagicMock()
-    mock_response.json.return_value = self.mock_petscan_response
-    mock_requests.get.return_value = mock_response
+  @patch('wp1.selection.models.petscan.requests.get')
+  def test_build_no_format(self, mock_requests_get):
+    mock_requests_get.return_value.text = self.mock_response
 
-    actual = self.builder.build('text/tab-separated-values',
+    self.builder.build('text/tab-separated-values',
                                 url='https://petscan.wmcloud.org.fake/?psid=42')
-    mock_requests.get.assert_called_with(
-        'https://petscan.wmcloud.org.fake/?psid=42&format=json',
+    mock_requests_get.assert_called_with(
+        'https://petscan.wmcloud.org.fake/?psid=42&format=plain',
         headers={
             'User-Agent': 'WP 1.0 bot 1.0.0/Audiodude <audiodude@gmail.com>'
         })
 
-  @patch('wp1.selection.models.petscan.requests')
-  def test_build_other_format(self, mock_requests):
-    mock_response = MagicMock()
-    mock_response.json.return_value = self.mock_petscan_response
-    mock_requests.get.return_value = mock_response
+  @patch('wp1.selection.models.petscan.requests.get')
+  def test_build_other_format(self, mock_requests_get):
+    mock_requests_get.return_value.text = self.mock_response
 
-    actual = self.builder.build(
+    self.builder.build(
         'text/tab-separated-values',
         url='https://petscan.wmcloud.org.fake/?psid=42&format=wiki')
-    mock_requests.get.assert_called_with(
-        'https://petscan.wmcloud.org.fake/?psid=42&format=json',
+    mock_requests_get.assert_called_with(
+        'https://petscan.wmcloud.org.fake/?psid=42&format=plain',
         headers={
             'User-Agent': 'WP 1.0 bot 1.0.0/Audiodude <audiodude@gmail.com>'
         })
 
   @patch('wp1.selection.models.petscan.requests.get')
   def test_build_non_200(self, mock_requests_get):
-    mock_response = MagicMock()
-    mock_response.raise_for_status.side_effect = requests.exceptions.HTTPError
-    mock_requests_get.return_value = mock_response
+    mock_requests_get.return_value.raise_for_status.side_effect = requests.exceptions.HTTPError
+    mock_requests_get.return_value.text = self.mock_response
 
     with self.assertRaises(Wp1FatalSelectionError):
-      actual = self.builder.build(
+      self.builder.build(
           'text/tab-separated-values',
           url='https://petscan.wmcloud.org.fake/?psid=42')
 
