fee48e5fb3ce0a793bc803d7c6b8cf536ab4db05
Arybhatt <bhattarya4533@gmail.com>
2026-02-27 21:13:12 +0530

    chore: resolve merge conflicts and simplify test description

--- wp1-frontend/component/Stats.cy.js
+++ wp1-frontend/component/Stats.cy.js
@@ -1,16 +1,8 @@
 import { mount } from '@cypress/vue'
-<<<<<<< HEAD
-
-=======
->>>>>>> f6a5520569b196b62d58f2632a042ede6a960ad3
 import Stats from '../../src/components/Stats.vue' 
 
 describe('Stats Component', () => {
-  it('renders correctly without backend API', () => {
-<<<<<<< HEAD
-=======
-   
->>>>>>> f6a5520569b196b62d58f2632a042ede6a960ad3
+  it('renders', () => {
     mount(Stats)
     cy.get('div').should('exist') 
   })
