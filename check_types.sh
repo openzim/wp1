7679120eeb1dd2b3dcfb09c814b2bcf5f04f8aa9
Travis Briggs <audiodude@gmail.com>
2026-05-24 06:56:59 -0700

    Add ty check step in CI, backed by local .sh script that developers can run themselves

---
+++ check_types.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# Run ty type checking against the subset of files that are fully annotated.
+#
+# Add a file to TYPED_FILES once it has type hints throughout. CI invokes
+# this script directly, so updates here are the single source of truth.
+#
+# Usage:
+#   ./check_types.sh           # check all files in TYPED_FILES
+#   ./check_types.sh wp1/foo.py  # check a specific file (overrides the list)
+
+set -e
+
+TYPED_FILES=(
+  wp1/models/wp10/builder.py
+  wp1/models/wp10/selection.py
+  wp1/models/wp10/zim_file.py
+  wp1/models/wp10/zim_schedule.py
+  wp1/logic/builder.py
+  wp1/logic/selection.py
+  wp1/logic/zim_files.py
+  wp1/logic/zim_schedules.py
+)
+
+if [ "$#" -gt 0 ]; then
+  ty check "$@"
+else
+  ty check "${TYPED_FILES[@]}"
+fi
